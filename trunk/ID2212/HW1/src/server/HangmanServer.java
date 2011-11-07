52bf00879b13b33872c9043adc81cec9f45625d0
julio.var@gmail.com <julio.var@gmail.com@4297f225-f6bb-08c7-5789-f87774b56954>
2011-11-07 18:33:23 +0000

    added some message handling

--- trunk/ID2212/HW1/src/server/HangmanServer.java
+++ trunk/ID2212/HW1/src/server/HangmanServer.java
@@ -15,9 +15,10 @@ import java.util.logging.Logger;
  * @author julio
  */
 public class HangmanServer {
-    private int port = 8080;
-    private int maxAttemptNumber = 6;
-    public void main(String args[]) {
+    public static void main(String args[]) {
+        int port = 8080;
+        int maxAttemptNumber = 6;
+
         if (args.length > 0) {
             try {
                 port = Integer.parseInt(args[0]);
@@ -26,14 +27,16 @@ public class HangmanServer {
             }
         }
 
+        System.out.println("Starting server...");
+        
         ServerSocket servSocket = null;
         try {
             servSocket = new ServerSocket(port);
             while (true) {
+                System.out.println("Waiting for incomming clients...");
                 Socket clientSocket = servSocket.accept();
                 (new HangmanHandler(clientSocket, maxAttemptNumber)).start();
             }
-
         } catch (IOException ex) {
             Logger.getLogger(HangmanServer.class.getName()).log(Level.SEVERE, null, ex);
         }
