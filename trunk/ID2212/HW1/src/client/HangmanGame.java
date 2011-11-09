d5a4fb1fc6944266cf836446a3055ad6bf2eb778
igor.kostyakov@gmail.com <igor.kostyakov@gmail.com@4297f225-f6bb-08c7-5789-f87774b56954>
2011-11-09 08:20:08 +0000

    client part (new server-total score issue)

--- trunk/ID2212/HW1/src/client/HangmanGame.java
+++ trunk/ID2212/HW1/src/client/HangmanGame.java
@@ -37,20 +37,21 @@ public class HangmanGame {
     private final static int LINGER = 100;
     //UI variable declaration
     private HangmanClient ui;
-
+    
     public HangmanGame(HangmanClient ui) {
         this.ui = ui;
     }
-
+    
     public void connect(String host, int port) {
         try {
             socket = new Socket(host, port);
             socket.setSoLinger(true, LINGER);
             out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
             in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-
+            
             ui.connectionEstablished();
             ui.updateTitle("Hangman --- Connected to " + host + ":" + port);
+            ui.updateTotalScore(0);
         } catch (UnknownHostException ex) {
             ui.updateTitle("Hangman");
             ui.reportConnectionError();
@@ -61,15 +62,15 @@ public class HangmanGame {
             //Logger.getLogger(HangmanGame.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-
+    
     public void connect(String host) {
         connect(host, DEFAULT_PORT);
     }
-
+    
     public void connect() {
         connect(DEFAULT_HOST, DEFAULT_PORT);
     }
-
+    
     public void disconnect() {
         if (socket != null) {
             try {
@@ -78,7 +79,7 @@ public class HangmanGame {
                 in.close();
                 out.close();
                 socket.close();
-
+                
                 ui.updateTitle("Hangman");
                 ui.connectionClosed();
             } catch (IOException ex) {
@@ -86,14 +87,14 @@ public class HangmanGame {
             }
         }
     }
-
+    
     public void newGame() {
         if (socket != null) {
             try {
                 out.write(MessageType.CLIENT_NEW_GAME.name());
                 out.newLine();
                 out.flush();
-
+                
                 String response;
                 if ((response = in.readLine()) != null) {
                     String[] msgParts = response.split("\t");
@@ -101,13 +102,13 @@ public class HangmanGame {
                     switch (type) {
                         case SERVER_NEW_GAME:
                             wordLength = Integer.parseInt(msgParts[1]);
-
+                            
                             char[] dashes = new char[wordLength];
                             Arrays.fill(dashes, '-');
                             wordView = new String(dashes);
-
+                            
                             attemptsLeft = Integer.parseInt(msgParts[2]);
-
+                            
                             playing = true;
 
                             //Update UI
@@ -115,7 +116,7 @@ public class HangmanGame {
                             ui.updateAttemptsLeftCount(attemptsLeft);
                             int stage = 0;
                             ui.updateHangmanImage(stage);
-
+                            
                             break;
                         default:
                         //TODO: server responded with wrong message
@@ -128,14 +129,14 @@ public class HangmanGame {
             }
         }
     }
-
+    
     public void guessLetter(char letter) {
         if ((socket != null) && playing) {
             try {
                 out.write(MessageType.CLIENT_GUESS_LETTER.name() + "\t" + letter);
                 out.newLine();
                 out.flush();
-
+                
                 String response;
                 if ((response = in.readLine()) != null) {
                     String[] msgParts = response.split("\t");
@@ -146,7 +147,7 @@ public class HangmanGame {
 
                             //Update UI
                             ui.updateWordView(wordView);
-
+                            
                             break;
                         case SERVER_WRONG_LETTER:
                             attemptsLeft = Integer.parseInt(msgParts[1]);
@@ -155,29 +156,29 @@ public class HangmanGame {
                             ui.updateAttemptsLeftCount(attemptsLeft);
                             int stage = MAX_ATTEMPTS - attemptsLeft;
                             ui.updateHangmanImage(stage);
-
+                            
                             break;
                         case SERVER_WIN:
                             wordView = msgParts[1];
                             totalScore = Integer.parseInt(msgParts[2]);
-
+                            
                             playing = false;
 
                             //Update UI
                             ui.updateWordView(wordView);
                             ui.reportWin();
                             ui.updateTotalScore(totalScore);
-
+                            
                             break;
                         case SERVER_GAME_OVER:
                             totalScore = Integer.parseInt(msgParts[1]);
-
+                            
                             playing = false;
 
                             //Update UI
                             ui.reportGameOver();
                             ui.updateTotalScore(totalScore);
-
+                            
                             break;
                         default:
                         //TODO: server responded with wrong message
@@ -190,14 +191,14 @@ public class HangmanGame {
             }
         }
     }
-
+    
     public void guessWord(String word) {
         if ((socket != null) && playing) {
             try {
                 out.write(MessageType.CLIENT_GUESS_WORD.name() + "\t" + word);
                 out.newLine();
                 out.flush();
-
+                
                 String response;
                 if ((response = in.readLine()) != null) {
                     String[] msgParts = response.split("\t");
@@ -206,24 +207,24 @@ public class HangmanGame {
                         case SERVER_WIN:
                             wordView = msgParts[1];
                             totalScore = Integer.parseInt(msgParts[2]);
-
+                            
                             playing = false;
 
                             //Update UI
                             ui.updateWordView(word);
                             ui.reportWin();
                             ui.updateTotalScore(totalScore);
-
+                            
                             break;
                         case SERVER_GAME_OVER:
                             totalScore = Integer.parseInt(msgParts[1]);
-
+                            
                             playing = false;
 
                             //Update UI
                             ui.reportGameOver();
                             ui.updateTotalScore(totalScore);
-
+                            
                             break;
                         default:
                         //TODO: server responded with wrong message
