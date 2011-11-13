6d1278058fecd0ca8dfc0200202c83acde2a0628
julio.var@gmail.com <julio.var@gmail.com@4297f225-f6bb-08c7-5789-f87774b56954>
2011-11-13 19:41:38 +0000

    upload hw2 project

---
+++ ID2212/HW2/src/Common/IMarketItem.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Common;
+
+/**
+ *
+ * @author julio
+ */
+public interface IMarketItem {
+    public Integer getMarketId();
+    public String getName();
+    public Integer getPrice();
+    
+}
