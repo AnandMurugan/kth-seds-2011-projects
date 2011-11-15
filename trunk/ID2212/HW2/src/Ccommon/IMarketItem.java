760de0c852e303e8e00be007a0cc391992578409
igor.kostyakov@gmail.com <igor.kostyakov@gmail.com@4297f225-f6bb-08c7-5789-f87774b56954>
2011-11-15 09:52:22 +0000

    step 1

---
+++ trunk/ID2212/HW2/src/Ccommon/IMarketItem.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Ccommon;
+
+/**
+ *
+ * @author julio
+ */
+public interface IMarketItem {
+    public Integer getMarketId();
+    public String getName();
+    public Integer getPrice();
+    
+}
