fa78a3d97dceed1ce70f8d92acf76e0f38738a5a
igor.kostyakov@gmail.com <igor.kostyakov@gmail.com@4297f225-f6bb-08c7-5789-f87774b56954>
2011-11-15 09:53:47 +0000

    step 2

---
+++ trunk/ID2212/HW2/src/common/RemoteClient.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package common;
+
+import server.MarketItem;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+/**
+ *
+ * @author julio
+ */
+public interface RemoteClient extends Remote {
+    public void notifyItemAvailable(MarketItem item) throws RemoteException;
+    public void notifyPurchaseSuccessful(MarketItem item) throws RemoteException;
+    //public void notifyPurchaseRejected(MarketItem item, RejectReason reason) throws RemoteException;
+    
+}
