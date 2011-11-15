4406934b0d8ab18305f7ef9a7004201dd07fe907
igor.kostyakov@gmail.com <igor.kostyakov@gmail.com@4297f225-f6bb-08c7-5789-f87774b56954>
2011-11-15 09:52:22 +0000

    step 1

---
+++ ID2212/HW2/src/Sserver/MarketImpl.java
@@ -0,0 +1,58 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Sserver;
+
+import Ccommon.IMarketItem;
+import Ccommon.RemoteClient;
+import Ccommon.RemoteMarket;
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.AbstractMap.SimpleEntry;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+/**
+ *
+ * @author julio
+ */
+public class MarketImpl extends UnicastRemoteObject implements RemoteMarket {
+    private List<MarketItem> wishList;
+    private Map<Integer, Entry<MarketItem, RemoteClient>> marketItems;
+    private String marketName;
+    private Integer currentItemId = 0;
+
+    public MarketImpl(String marketName) throws RemoteException {
+        super();
+        this.marketName = marketName;
+
+    }
+
+    @Override
+    public IMarketItem addItemToMarket(String itemName, Integer itemPrice, RemoteClient seller) throws RemoteException {
+        currentItemId++;
+        MarketItem item = new MarketItem(currentItemId, itemName, itemPrice);
+        marketItems.put(item.getMarketId(), new SimpleEntry(item, seller));
+        return item;
+    }
+
+    @Override
+    public IMarketItem addItemToWishList(String itemName, Integer itemPrice, RemoteClient buyer) throws RemoteException {
+        currentItemId++;
+        MarketItem item = new MarketItem(currentItemId, itemName, itemPrice);
+        marketItems.put(item.getMarketId(), new SimpleEntry(item, buyer));
+        return item;
+    }
+
+    @Override
+    public List<IMarketItem> getAvailableItems() throws RemoteException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void buyItem(MarketItem item, RemoteClient buyer) throws RemoteException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+}
