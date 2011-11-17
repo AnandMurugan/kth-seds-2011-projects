c68c88641ebb74c61d2101e6426b88e820e09516
igor.kostyakov@gmail.com <igor.kostyakov@gmail.com@4297f225-f6bb-08c7-5789-f87774b56954>
2011-11-17 11:58:57 +0000

    initial framework of ArtistManagerAgent

---
+++ ID2209/HW2/src/model/Artifact.java
@@ -0,0 +1,74 @@
+package model;
+
+import java.io.Serializable;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author Igor
+ */
+public class Artifact implements Serializable {
+    private static long idCounter = 1;
+    private long id;
+    private String name;
+    private String creator;
+    private String description;
+    private String style;
+    private String museum;
+
+    public Artifact(String name, String creator, String description, String style, String museum) {
+        this.id = idCounter++;
+        this.name = name;
+        this.creator = creator;
+        this.description = description;
+        this.style = style;
+        this.museum = museum;
+    }
+
+    public String getCreator() {
+        return creator;
+    }
+
+    public void setCreator(String creator) {
+        this.creator = creator;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getMuseum() {
+        return museum;
+    }
+
+    public void setMuseum(String museum) {
+        this.museum = museum;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStyle() {
+        return style;
+    }
+
+    public void setStyle(String style) {
+        this.style = style;
+    }
+}
