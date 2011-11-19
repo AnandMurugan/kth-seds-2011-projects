579de6ead3e26027c4539b7bc6838e676de44460
julio.var@gmail.com <julio.var@gmail.com@4297f225-f6bb-08c7-5789-f87774b56954>
2011-11-19 15:05:39 +0000

    Added interfaces

---
+++ trunk/IK2206/Seminar/src/common/IContainerFile.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package common;
+
+/**
+ *
+ * @author julio
+ */
+public interface IContainerFile {
+    void loadFile();
+    byte getNextByte();
+}
