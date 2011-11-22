84b5a34351a6e7c0838099654fee6aa338ea86ec
igor.kostyakov@gmail.com <igor.kostyakov@gmail.com@4297f225-f6bb-08c7-5789-f87774b56954>
2011-11-22 10:54:22 +0000

    ID2209_HW3: creating repo folder (just a copy of HW2)

---
+++ trunk/ID2209/HW3/src/agents/ArtistManagerAgent.java
@@ -0,0 +1,316 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package agents;
+
+import gui.ArtistManagerFrame;
+import gui.ArtistManagerResponsiveGUI;
+import jade.core.AID;
+import jade.core.Agent;
+import jade.core.behaviours.Behaviour;
+import jade.core.behaviours.FSMBehaviour;
+import jade.core.behaviours.OneShotBehaviour;
+import jade.domain.DFService;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.domain.FIPAException;
+import jade.gui.GuiAgent;
+import jade.gui.GuiEvent;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
+import java.awt.EventQueue;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import model.Artifact;
+
+/**
+ *
+ * @author Igor
+ */
+public class ArtistManagerAgent extends GuiAgent {
+    private transient ArtistManagerResponsiveGUI gui;
+    public static final int QUIT = 0;
+    public static final int START_AUCTION = 1;
+
+    @Override
+    protected void setup() {
+        //Register the service in DF
+        DFAgentDescription dfd = new DFAgentDescription();
+        dfd.setName(getAID());
+        ServiceDescription sd = new ServiceDescription();
+        sd.setType("artist-manager");
+        sd.setName(getAID().getName() + "_artist-manager");
+        dfd.addServices(sd);
+        try {
+            DFService.register(this, dfd);
+
+            /* Create and display the form */
+            final ArtistManagerAgent agent = this;
+            EventQueue.invokeLater(new Runnable() {
+                @Override
+                public void run() {
+                    gui = new ArtistManagerFrame(agent);
+                    gui.open();
+                }
+            });
+        } catch (FIPAException ex) {
+            doDelete();
+        }
+
+        System.out.println("Artist Manager [" + getAID().getLocalName() + "] is ready...");
+//        try {
+//            Thread.sleep(1000);
+//            sellArtifact(new String[]{"Art", "Artman", "Just simple art", "cubism", "150", "50"});
+//        } catch (InterruptedException ex) {
+//            Logger.getLogger(ArtistManagerAgent.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+    }
+
+    @Override
+    protected void takeDown() {
+        try {
+            System.out.println("Artist Manager [" + getAID().getLocalName() + "] is terminating...");
+            DFService.deregister(this);
+        } catch (FIPAException ex) {
+            Logger.getLogger(ArtistManagerAgent.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public void sellArtifact(String[] args) {
+        String name = args[0];
+        String creator = args[1];
+        String description = args[2];
+        String style = args[3];
+        float initialPrice = Float.parseFloat(args[4]);
+        float reservePrice = Float.parseFloat(args[5]);
+
+        Artifact a = new Artifact(name, creator, description, style);
+
+        addBehaviour(new DutchAuctionInitiatorBehaviour(this, a, initialPrice, reservePrice));
+    }
+
+    @Override
+    protected void onGuiEvent(GuiEvent ge) {
+        switch (ge.getType()) {
+            case QUIT:
+                gui.close();
+                doDelete();
+                break;
+            case START_AUCTION:
+                String[] args = (String[]) ge.getParameter(0);
+                sellArtifact(args);
+                break;
+        }
+    }
+
+    private class DutchAuctionInitiatorBehaviour extends FSMBehaviour {
+        private static final int ROUNDS = 200;
+        private String conversationId;
+        private AID[] curators;
+        private AID winner;
+        private List<AID> runnerUps = new ArrayList<AID>();
+        private MessageTemplate auctionTemplate;
+        private float reductionRate;
+        private float currentPrice;
+        private int responsesCount;
+
+        public DutchAuctionInitiatorBehaviour(final Agent agent, final Artifact artifact, final float initialPrice, final float reservePrice) {
+            super(agent);
+
+            curators = new AID[0];
+            conversationId = "dutch_" + System.currentTimeMillis();
+            auctionTemplate = MessageTemplate.MatchConversationId(conversationId);
+            reductionRate = (initialPrice - reservePrice) / (ROUNDS - 1);
+            currentPrice = initialPrice;
+
+
+            //Discover all curators
+            registerFirstState(new OneShotBehaviour(agent) {
+                @Override
+                public void action() {
+                    gui.log("Searching for curators...");//System.out.println(myAgent.getAID().getName() + " is searching for tour-guides...");
+                    DFAgentDescription template = new DFAgentDescription();
+                    ServiceDescription sd = new ServiceDescription();
+                    sd.setType("curator");
+                    template.addServices(sd);
+                    try {
+                        DFAgentDescription[] result = DFService.search(myAgent, template);
+                        if (result.length == 0) {
+                            gui.log("No curators were found...");//System.out.println(myAgent.getAID().getName() + ": ERROR - No tour-guides found...");
+                            return;
+                        }
+
+                        gui.log("Following curators were found:");//System.out.println(myAgent.getAID().getName() + " has found the next tour-guides:");
+                        curators = new AID[result.length];
+                        for (int i = 0; i < result.length; ++i) {
+                            curators[i] = result[i].getName();
+                            gui.log("\t" + curators[i].getLocalName());//System.out.println("\t" + tourGuideAgents[i].getName());
+                        }
+                    } catch (FIPAException ex) {
+                        Logger.getLogger(ArtistManagerAgent.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+
+                @Override
+                public int onEnd() {
+                    return curators.length == 0 ? 0 : 1;
+                }
+            }, "SEARCHING_CURATORS");
+
+            //
+            registerLastState(new OneShotBehaviour(agent) {
+                @Override
+                public void action() {
+                    gui.log("Auction is terminating due to absence of any curator...\n");
+                }
+            }, "NO_CURATORS");
+            //
+            registerLastState(new OneShotBehaviour(agent) {
+                @Override
+                public void action() {
+                    //Respond to all curators
+                    ACLMessage informMsg = new ACLMessage(ACLMessage.INFORM);
+                    informMsg.setConversationId(conversationId);
+                    informMsg.setContent("terminating");
+                    for (AID aid : curators) {
+                        informMsg.addReceiver(aid);
+                    }
+                    myAgent.send(informMsg);
+
+                    //Log
+                    gui.log("Auction is terminating...");
+                }
+            }, "TERMINATING_AUCTION");
+
+            //
+            registerState(new OneShotBehaviour(agent) {
+                @Override
+                public void action() {
+                    //Inform all curators about artifact
+                    ACLMessage informMsg = new ACLMessage(ACLMessage.INFORM);
+                    informMsg.setConversationId(conversationId);
+                    informMsg.setContent(artifact.getStyle());
+                    for (AID aid : curators) {
+                        informMsg.addReceiver(aid);
+                    }
+                    myAgent.send(informMsg);
+
+                    //Log
+                    gui.log("Informing curators about start of auction...");
+                }
+            }, "INFORMING_CURATORS");
+            //
+            registerState(new OneShotBehaviour(agent) {
+                @Override
+                public void action() {
+                    //Log
+                    gui.log("Sending curators cfp [price=" + currentPrice + "]...");
+
+                    //Inform all curators about artifact
+                    ACLMessage cfpMsg = new ACLMessage(ACLMessage.CFP);
+                    cfpMsg.setConversationId(conversationId);
+                    cfpMsg.setContent(Float.toString(currentPrice));
+                    for (AID aid : curators) {
+                        cfpMsg.addReceiver(aid);
+                        gui.log("\tSending cfp to [" + aid.getLocalName() + "]");
+                    }
+                    myAgent.send(cfpMsg);
+                    gui.log("Collecting responses...");
+                    responsesCount = 0;
+                }
+            }, "CFP");
+            //
+            registerState(new Behaviour(agent) {
+                @Override
+                public void action() {
+                    ACLMessage response = myAgent.receive(auctionTemplate);
+                    if (response != null) {
+                        switch (response.getPerformative()) {
+                            case ACLMessage.PROPOSE:
+                                if (winner == null) {
+                                    winner = response.getSender();
+                                } else {
+                                    runnerUps.add(response.getSender());
+                                }
+                                break;
+                            case ACLMessage.NOT_UNDERSTOOD:
+                                break;
+                        }
+                        responsesCount++;
+                        gui.log("\t#" + responsesCount + " [" + response.getSender().getLocalName() + "] responded with " + ACLMessage.getPerformative(response.getPerformative()));
+                    } else {
+                        block();
+                    }
+                }
+
+                @Override
+                public boolean done() {
+                    return responsesCount == curators.length;
+                }
+
+                @Override
+                public int onEnd() {
+                    return winner != null ? 1 : 0;
+                }
+            }, "COLLECTING_PROPOSALS");
+            //
+            registerState(new OneShotBehaviour(agent) {
+                @Override
+                public void action() {
+                    gui.log("Planning next round...");
+                    currentPrice -= reductionRate;
+                }
+
+                @Override
+                public int onEnd() {
+                    return Float.compare(currentPrice + 0.0001f, reservePrice) <= 0 ? 1 : 0;
+                }
+            }, "PLANNING_NEXT_ROUND");
+            //
+            registerState(new OneShotBehaviour(agent) {
+                @Override
+                public void action() {
+                    //Respond to the winner
+                    try {
+                        ACLMessage acceptMsg = new ACLMessage(ACLMessage.ACCEPT_PROPOSAL);
+                        acceptMsg.setConversationId(conversationId);
+                        acceptMsg.setContentObject(artifact);
+                        acceptMsg.addReceiver(winner);
+                        myAgent.send(acceptMsg);
+                    } catch (IOException ex) {
+                        Logger.getLogger(ArtistManagerAgent.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+
+                    //Respond to the runner-ups
+                    ACLMessage rejectMsg = new ACLMessage(ACLMessage.REJECT_PROPOSAL);
+                    rejectMsg.setConversationId(conversationId);
+                    for (AID runnerUp : runnerUps) {
+                        rejectMsg.addReceiver(runnerUp);
+                    }
+                    myAgent.send(rejectMsg);
+
+                    //Log
+                    gui.log("Auction is succesful:");
+                    gui.log("\tWinner is [" + winner.getLocalName() + "]");
+                    for (AID runnerUp : runnerUps) {
+                        gui.log("\tRunner-up is [" + runnerUp.getLocalName() + "]");
+                    }
+                }
+            }, "FINISHING_AUCTION");
+
+            registerTransition("SEARCHING_CURATORS", "NO_CURATORS", 0);
+            registerTransition("SEARCHING_CURATORS", "INFORMING_CURATORS", 1);
+            registerDefaultTransition("INFORMING_CURATORS", "CFP");
+            registerDefaultTransition("CFP", "COLLECTING_PROPOSALS");
+            registerTransition("COLLECTING_PROPOSALS", "PLANNING_NEXT_ROUND", 0);
+            registerTransition("COLLECTING_PROPOSALS", "FINISHING_AUCTION", 1);
+            registerTransition("PLANNING_NEXT_ROUND", "CFP", 0);
+            registerTransition("PLANNING_NEXT_ROUND", "TERMINATING_AUCTION", 1);
+            registerDefaultTransition("FINISHING_AUCTION", "TERMINATING_AUCTION");
+        }
+    }
+}
---
+++ trunk/ID2209/HW3/src/agents/CuratorAgent.java
@@ -0,0 +1,311 @@
+package agents;
+
+import jade.core.Agent;
+import jade.core.behaviours.Behaviour;
+import jade.core.behaviours.FSMBehaviour;
+import jade.domain.DFService;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.domain.FIPAException;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
+import jade.lang.acl.UnreadableException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import model.Artifact;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author Igor
+ */
+public class CuratorAgent extends Agent {
+    private String museum;
+    private String preferedStyle;
+    private List<Artifact> artifacts = new ArrayList<Artifact>();
+    private static long counter = 1;
+
+    @Override
+    protected void setup() {
+        //Setting the museum name
+        Object[] args = getArguments();
+        if (args != null && args.length > 1) {
+            museum = (String) args[0];
+            preferedStyle = (String) args[1];
+        } else {
+            doDelete();
+        }
+
+        System.out.println("Hello! Curator " + getAID().getName() + " in museum " + museum + " is ready...");
+
+        //Register service in DF
+        DFAgentDescription dfd = new DFAgentDescription();
+        dfd.setName(getAID());
+        ServiceDescription sd = new ServiceDescription();
+        sd.setType("curator");
+        sd.setName(museum + "_curator" + (counter++));
+        dfd.addServices(sd);
+        try {
+            DFService.register(this, dfd);
+        } catch (FIPAException ex) {
+            ex.printStackTrace();
+        }
+
+        addBehaviour(new DutchAuctionParticipantBehaviour());
+    }
+
+    @Override
+    protected void takeDown() {
+        super.takeDown();
+    }
+
+    class DutchAuctionParticipantBehaviour extends FSMBehaviour {
+        private MessageTemplate msgTemplate;
+        private Random rand;
+        private float price;
+        private float maxPrice;
+        private String style;
+        // Define states
+        private static final String WAIT_AUCTION_STATE = "wait-auction";
+        private static final String WAIT_CFP_OR_TERMINATE_STATE = "wait-cfp-or-terminate";
+        private static final String WAIT_PROPOSAL_REPLY_STATE = "wait-proposal-reply";
+        private static final String END_AUCTION_STATE = "end-auction";
+        private static final String END_AUCTION_NO_BIDS_STATE = "end-auction-no-bids";
+        // Define transitions
+        private final int NEW_AUCTION_TRANSITION = 1;
+        private final int NO_BIDS_TRANSITION = 2;
+        private final int SENT_PROPOSAL_TRANSITION = 3;
+        private final int NOT_GOOD_PRICE_TRANSITION = 4;
+        private final int RECEIVED_PROPOSAL_REPLY_TRANSITION = 5;
+        private final int DEFAULT_ERROR_STATE = 5;
+
+        @Override
+        public void onStart() {
+            msgTemplate = MessageTemplate.MatchPerformative(ACLMessage.INFORM);
+            rand = new Random(System.nanoTime());
+
+            // Register states
+            registerFirstState(new WaitAuctionBehaviour(myAgent), WAIT_AUCTION_STATE);
+            registerState(new WaitCfpBehaviour(myAgent), WAIT_CFP_OR_TERMINATE_STATE);
+            registerState(new WaitReplyBehaviour(myAgent), WAIT_PROPOSAL_REPLY_STATE);
+            registerLastState(new WaitEndAuctionBehaviour(myAgent), END_AUCTION_STATE);
+            registerLastState(new NoBidsEndAuctionBehaviour(myAgent), END_AUCTION_NO_BIDS_STATE);
+
+            // Register Transitions
+            registerTransition(WAIT_AUCTION_STATE, WAIT_CFP_OR_TERMINATE_STATE, NEW_AUCTION_TRANSITION);
+            registerTransition(WAIT_CFP_OR_TERMINATE_STATE, WAIT_PROPOSAL_REPLY_STATE, SENT_PROPOSAL_TRANSITION);
+            registerTransition(WAIT_CFP_OR_TERMINATE_STATE, WAIT_CFP_OR_TERMINATE_STATE, NOT_GOOD_PRICE_TRANSITION);
+            registerTransition(WAIT_CFP_OR_TERMINATE_STATE, END_AUCTION_STATE, DEFAULT_ERROR_STATE);
+            registerTransition(WAIT_CFP_OR_TERMINATE_STATE, END_AUCTION_NO_BIDS_STATE, NO_BIDS_TRANSITION);
+            registerTransition(WAIT_PROPOSAL_REPLY_STATE, END_AUCTION_STATE, RECEIVED_PROPOSAL_REPLY_TRANSITION);
+        }
+
+        private class WaitAuctionBehaviour extends Behaviour {
+            boolean notWaitAuction = false;
+
+            WaitAuctionBehaviour(Agent aAgent) {
+                super(aAgent);
+            }
+
+            @Override
+            public void action() {
+                ACLMessage initAuctionMsg = myAgent.receive(msgTemplate);
+                if (initAuctionMsg != null) {
+                    notWaitAuction = true;
+                    style = initAuctionMsg.getContent();
+                    System.out.println(getAID().getName() + " Started new auction...");
+                    msgTemplate = MessageTemplate.and(
+                            MessageTemplate.or(MessageTemplate.MatchPerformative(ACLMessage.CFP), MessageTemplate.MatchPerformative(ACLMessage.INFORM)),
+                            MessageTemplate.MatchConversationId(initAuctionMsg.getConversationId()));
+                } else {
+                    block();
+                }
+            }
+
+            @Override
+            public boolean done() {
+                return notWaitAuction;
+            }
+
+            @Override
+            public int onEnd() {
+                return NEW_AUCTION_TRANSITION;
+            }
+        }
+
+        private class WaitCfpBehaviour extends Behaviour {
+            boolean notWaitCfp = false;
+            int transition = DEFAULT_ERROR_STATE;
+
+            WaitCfpBehaviour(Agent aAgent) {
+                super(aAgent);
+            }
+
+            @Override
+            public void action() {
+                ACLMessage cfpMsg = myAgent.receive(msgTemplate);
+                if (cfpMsg != null) {
+                    notWaitCfp = true;
+                    System.out.println(getAID().getName() + " received a message from art agent...");
+                    if (cfpMsg.getPerformative() == ACLMessage.CFP) {
+                        price = Float.parseFloat(cfpMsg.getContent());
+                        if (maxPrice <= 0) {
+                            float low = 0.5f;
+                            maxPrice = price * (low + rand.nextFloat() * (1.0f - low));
+                        }
+                        System.out.println(getAID().getName() + " processing CFP message.");
+                        boolean satisfactoryPrice = evaluate(style, price);
+
+                        if (satisfactoryPrice) {
+                            System.out.println(getAID().getName() + " good price.");
+                            ACLMessage proposalMsg = new ACLMessage(ACLMessage.PROPOSE);
+                            proposalMsg.addReceiver(cfpMsg.getSender());
+                            proposalMsg.setConversationId(cfpMsg.getConversationId());
+                            myAgent.send(proposalMsg);
+
+                            transition = SENT_PROPOSAL_TRANSITION;
+
+                            msgTemplate = MessageTemplate.and(
+                                    MessageTemplate.or(MessageTemplate.MatchPerformative(ACLMessage.ACCEPT_PROPOSAL), MessageTemplate.MatchPerformative(ACLMessage.REJECT_PROPOSAL)),
+                                    MessageTemplate.MatchConversationId(cfpMsg.getConversationId()));
+                        } else {
+                            System.out.println(getAID().getName() + " not convenient price.");
+                            ACLMessage proposalMsg = new ACLMessage(ACLMessage.NOT_UNDERSTOOD);
+                            proposalMsg.addReceiver(cfpMsg.getSender());
+                            proposalMsg.setConversationId(cfpMsg.getConversationId());
+                            myAgent.send(proposalMsg);
+
+                            transition = NOT_GOOD_PRICE_TRANSITION;
+
+                            msgTemplate = MessageTemplate.and(
+                                    MessageTemplate.or(MessageTemplate.MatchPerformative(ACLMessage.CFP), MessageTemplate.MatchPerformative(ACLMessage.INFORM)),
+                                    MessageTemplate.MatchConversationId(cfpMsg.getConversationId()));
+                        }
+                    } else if (cfpMsg.getPerformative() == ACLMessage.INFORM) {
+                        transition = NO_BIDS_TRANSITION;
+                        System.out.println(getAID().getName() + " no bids.");
+
+                        msgTemplate = MessageTemplate.and(MessageTemplate.MatchPerformative(ACLMessage.INFORM),
+                                MessageTemplate.MatchConversationId(cfpMsg.getConversationId()));
+                    }
+                } else {
+                    block();
+                }
+            }
+
+            @Override
+            public boolean done() {
+                return notWaitCfp;
+            }
+
+            @Override
+            public int onEnd() {
+                return transition;
+            }
+
+            private boolean evaluate(String style, float price) {
+                if (!preferedStyle.equals(style)) {
+                    return false;
+                } else {
+                    if (Float.compare(maxPrice, price) > 0) {
+                        double coin = rand.nextDouble();
+                        return coin > 0.5;
+                    } else {
+                        return false;
+                    }
+                }
+            }
+        }
+
+        private class WaitReplyBehaviour extends Behaviour {
+            boolean gotReply = false;
+            int transition = DEFAULT_ERROR_STATE;
+
+            WaitReplyBehaviour(Agent aAgent) {
+                super(aAgent);
+            }
+
+            @Override
+            public void action() {
+                ACLMessage replyMsg = myAgent.receive(msgTemplate);
+                if (replyMsg != null) {
+                    gotReply = true;
+                    transition = RECEIVED_PROPOSAL_REPLY_TRANSITION;
+                    if (replyMsg.getPerformative() == ACLMessage.ACCEPT_PROPOSAL) {
+                        try {
+                            artifacts.add((Artifact) replyMsg.getContentObject());
+                        } catch (UnreadableException ex) {
+                            Logger.getLogger(CuratorAgent.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+                        System.out.println(getAID().getName() + " Bid accepted. ");
+                    } else if (replyMsg.getPerformative() == ACLMessage.REJECT_PROPOSAL) {
+                        System.out.println(getAID().getName() + " Bid rejected, someone else bought the item. ");
+                    }
+
+                    msgTemplate = MessageTemplate.and(MessageTemplate.MatchPerformative(ACLMessage.INFORM),
+                            MessageTemplate.MatchConversationId(replyMsg.getConversationId()));
+                } else {
+                    block();
+                }
+            }
+
+            @Override
+            public boolean done() {
+                return gotReply;
+            }
+
+            @Override
+            public int onEnd() {
+                return transition;
+            }
+        }
+
+        private class WaitEndAuctionBehaviour extends Behaviour {
+            boolean gotReply = false;
+
+            WaitEndAuctionBehaviour(Agent aAgent) {
+                super(aAgent);
+            }
+
+            @Override
+            public void action() {
+                ACLMessage replyMsg = myAgent.receive(msgTemplate);
+                if (replyMsg != null) {
+                    gotReply = true;
+                    System.out.println(getAID().getName() + " Auction finished. ");
+                    myAgent.addBehaviour(new DutchAuctionParticipantBehaviour());
+                } else {
+                    block();
+                }
+            }
+
+            @Override
+            public boolean done() {
+                return gotReply;
+            }
+        }
+
+        private class NoBidsEndAuctionBehaviour extends Behaviour {
+            NoBidsEndAuctionBehaviour(Agent aAgent) {
+                super(aAgent);
+            }
+
+            @Override
+            public void action() {
+                System.out.println(getAID().getName() + " Auction finished. ");
+                myAgent.addBehaviour(new DutchAuctionParticipantBehaviour());
+            }
+
+            @Override
+            public boolean done() {
+                return true;
+            }
+        }
+    }
+}
