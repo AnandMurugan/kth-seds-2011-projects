19e0b7f4d5e07ee041e26920e901f59a10faf5a2
julio.var@gmail.com <julio.var@gmail.com@4297f225-f6bb-08c7-5789-f87774b56954>
2011-11-24 09:22:58 +0000

    Contract modified

--- ID2212/HW3/src/server/FileCatalog.java
+++ ID2212/HW3/src/server/FileCatalog.java
@@ -4,7 +4,10 @@
  */
 package server;
 
+import java.io.File;
 import java.rmi.Remote;
+import java.util.List;
+import model.CatalogFile;
 import model.CatalogUser;
 
 /**
@@ -15,11 +18,13 @@ public interface FileCatalog extends Remote {
     void registerUser(String name, String pwd);
     void unRegisterUser(String name, String pwd);
     CatalogUser login(String name, String pwd);
+    void logout( String name, String pwd);
     // TODO. Add parameters 
-    void uploadFile();
-    void downloadFile();
-    void deleteFile();
-    void updateFile();
-            
+    void uploadFile(String userName, CatalogFile fileDescription, File file);
+    File downloadFile(long fileId, String userName);
+    void deleteFile(long fileId, String userName);
+    void updateFile(long fileId, File file, String userName);
     
+    List<CatalogFile> getAllFiles();
+    List<CatalogFile> getMyFiles(String userName);
 }
