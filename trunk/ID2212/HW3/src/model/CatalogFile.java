d683a494d311ac18dfbc7d0138e9a221d18cd83b
igor.kostyakov@gmail.com <igor.kostyakov@gmail.com@4297f225-f6bb-08c7-5789-f87774b56954>
2011-11-24 07:25:25 +0000

    ID2212_HW3:

--- trunk/ID2212/HW3/src/model/CatalogFile.java
+++ trunk/ID2212/HW3/src/model/CatalogFile.java
@@ -4,38 +4,113 @@
  */
 package model;
 
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
 /**
  *
  * @author julio
  */
-
-
-public class CatalogFile {
-    
-    private String name;
-    private int size;
+@Entity
+public class CatalogFile implements Serializable {
+    @Id
+    @GeneratedValue
+    private int id;
+    private String fileName;
+    private int fileSize;
+    @ManyToOne
     private CatalogUser owner;
-    private AccessPermission permission;
-    private long id;
-    private String path;
-    
-    public long getId(){
+    @Enumerated(EnumType.STRING)
+    private AccessPermission accessPermission;
+    @Temporal(TemporalType.DATE)
+    private Date lastModifiedTime;
+    @Enumerated(EnumType.STRING)
+    private WriteReadPermission writeReadPermission;
+    private String filePath;
+
+    public CatalogFile() {
+    }
+
+    public CatalogFile(int id, String fileName, int fileSize, CatalogUser owner, AccessPermission accessPermission, Date lastModifiedTime, WriteReadPermission writeReadPermission, String filePath) {
+        this.id = id;
+        this.fileName = fileName;
+        this.fileSize = fileSize;
+        this.owner = owner;
+        this.accessPermission = accessPermission;
+        this.lastModifiedTime = lastModifiedTime;
+        this.writeReadPermission = writeReadPermission;
+        this.filePath = filePath;
+    }
+
+    public AccessPermission getAccessPermission() {
+        return accessPermission;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public int getFileSize() {
+        return fileSize;
+    }
+
+    public int getId() {
         return id;
     }
-    
-    public String getName(){
-        return name;
-    } 
-    
-    public int getSize(){
-        return size;
-    } 
-    
-    public CatalogUser getOwner(){
+
+    public Date getLastModifiedTime() {
+        return lastModifiedTime;
+    }
+
+    public CatalogUser getOwner() {
         return owner;
     }
-    
-    public AccessPermission getAccessPermission(){
-        return permission;
+
+    public WriteReadPermission getWriteReadPermission() {
+        return writeReadPermission;
+    }
+
+    public void setAccessPermission(AccessPermission accessPermission) {
+        this.accessPermission = accessPermission;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }
+
+    public void setFileSize(int fileSize) {
+        this.fileSize = fileSize;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setLastModifiedTime(Date lastModifiedTime) {
+        this.lastModifiedTime = lastModifiedTime;
+    }
+
+    public void setOwner(CatalogUser owner) {
+        this.owner = owner;
+    }
+
+    public void setWriteReadPermission(WriteReadPermission writeReadPermission) {
+        this.writeReadPermission = writeReadPermission;
     }
 }
