dee368dcf0f939761d0f9784032937b966f4af4d
igor.kostyakov@gmail.com <igor.kostyakov@gmail.com@4297f225-f6bb-08c7-5789-f87774b56954>
2011-11-24 07:25:25 +0000

    ID2212_HW3:

--- ID2212/HW3/src/model/CatalogUser.java
+++ ID2212/HW3/src/model/CatalogUser.java
@@ -4,25 +4,65 @@
  */
 package model;
 
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
 /**
  *
  * @author julio
  */
-public class CatalogUser {
-    private String userName;
-    private String password;
-    
-    public CatalogUser(String name, String pwd){
-        userName = name;
-        this.password = pwd;
+@Entity
+public class CatalogUser implements Serializable {
+    @Id
+    @GeneratedValue
+    private int id;
+    private String name;
+    private int password;
+    @OneToMany(mappedBy = "owner")
+    private List<CatalogFile> myCatalogFiles;
+
+    public CatalogUser() {
     }
-    
-    
-    public String getUserName(){
-        return userName;
+
+    public CatalogUser(int id, String name, int password) {
+        this.id = id;
+        this.name = name;
+        this.password = password;
     }
-    
-    public String getPassword(){
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPassword() {
         return password;
+    }
+
+    public List<CatalogFile> getMyCatalogFiles() {
+        return myCatalogFiles;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPassword(int password) {
+        this.password = password;
+    }
+
+    public void setMyCatalogFiles(List<CatalogFile> myCatalogFiles) {
+        this.myCatalogFiles = myCatalogFiles;
     }
 }
