ffc6cabc1836f1f65c6eb2b4944cd5f633b3d468
igor.kostyakov@gmail.com <igor.kostyakov@gmail.com@4297f225-f6bb-08c7-5789-f87774b56954>
2011-11-25 07:04:33 +0000

    ID2212_HW3

--- trunk/ID2212/HW3/src/client/CatalogClientImp.java
+++ trunk/ID2212/HW3/src/client/CatalogClientImp.java
@@ -8,6 +8,7 @@ import java.net.MalformedURLException;
 import java.rmi.Naming;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
+import java.util.ArrayList;
 import java.util.List;
 import model.AccessPermission;
 import model.CatalogFile;
@@ -178,6 +179,8 @@ public class CatalogClientImp implements CatalogClient {
 
         try {
             //catalog.logout(this.currentUserId);
+            ui.updateAllFiles(new ArrayList<CatalogFile>());
+            ui.updateMyFiles(new ArrayList<CatalogFile>());
             ui.updateAfterLogout();
             catalog.unregisterUser(currentUserId);
         } catch (RejectedException ex) {
