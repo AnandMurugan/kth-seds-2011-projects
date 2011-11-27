d18f2d90fb946627badec5cabddff849b31367cd
igor.kostyakov@gmail.com <igor.kostyakov@gmail.com@4297f225-f6bb-08c7-5789-f87774b56954>
2011-11-27 23:24:41 +0000

    IK2206_Seminar

--- trunk/IK2206/Seminar/src/stegoStrategy/RandomSeqSimpleLSB.java
+++ trunk/IK2206/Seminar/src/stegoStrategy/RandomSeqSimpleLSB.java
@@ -1,9 +1,7 @@
 package stegoStrategy;
 
 import java.util.Iterator;
-import java.util.Random;
 import java.util.Set;
-import java.util.TreeSet;
 
 import stegoFile.ByteHelper;
 
@@ -13,50 +11,50 @@ import common.IStegoStrategy;
 import common.IWriteStegoFile;
 
 public class RandomSeqSimpleLSB implements IStegoStrategy {
-	private int nrOfBits;
-	private int seed;
-	
-	public RandomSeqSimpleLSB(int nrOfBits, int seed) {
-		this.nrOfBits = nrOfBits;
-		this.seed = seed;
-	}
-	
-	public void encode(IContainerFile cover, IReadStegoFile stego) {
-		System.out.println("encoding...");
-		RandomUnique randGen = new RandomUnique(seed, cover.getSize());
-		Set<Integer> indexValues = randGen.nextInt(stego.getSize()/nrOfBits);
-		Iterator<Integer> it = indexValues.iterator();
-		while(it.hasNext()) {
-			int nextIndex = it.next();
-			
-			byte coverByte = cover.getByte(nextIndex);
-			byte stegoByte = stego.getNextBits(nrOfBits);
-			coverByte = (byte) (coverByte >> nrOfBits);
-			coverByte = (byte) (coverByte << nrOfBits);
-			coverByte = (byte) (coverByte | stegoByte);
-			cover.setByte(nextIndex, coverByte);
-		}
-		if(stego.hasMoreBits()) {
-			System.out.println("Couldn't encode the whole message, container was too small");
-		}
-	}
+    private int nrOfBits;
+    private int seed;
 
-	public void decode(IContainerFile cover, IWriteStegoFile stego) {
-		System.out.println("decoding...");
-		
-		RandomUnique randGen = new RandomUnique(seed, cover.getSize());
-		Set<Integer> indexValues = randGen.nextInt(stego.getSize()/nrOfBits);
-		Iterator<Integer> it = indexValues.iterator();
-		while(it.hasNext()) {
-			int nextIndex = it.next();
-		
-			byte coverByte = cover.getByte(nextIndex);
-			short mask = ByteHelper.getMask(nrOfBits-1, nrOfBits);
-			byte stegoByte = (byte)(coverByte & mask);
-			stego.setNextBits(stegoByte, nrOfBits);
-		}
-		if(stego.hasMoreBits()) {
-			System.out.println("Couldn't decode the whole message, container was too small");
-		}
-	}
+    public RandomSeqSimpleLSB(int nrOfBits, int seed) {
+        this.nrOfBits = nrOfBits;
+        this.seed = seed;
+    }
+
+    public void encode(IContainerFile cover, IReadStegoFile stego) {
+        System.out.println("encoding...");
+        RandomUnique randGen = new RandomUnique(seed, cover.getSize());
+        Set<Integer> indexValues = randGen.nextInt(stego.getSize() / nrOfBits);
+        Iterator<Integer> it = indexValues.iterator();
+        while (it.hasNext()) {
+            int nextIndex = it.next();
+
+            byte coverByte = cover.getByte(nextIndex);
+            byte stegoByte = stego.getNextBits(nrOfBits);
+            coverByte = (byte) (coverByte >> nrOfBits);
+            coverByte = (byte) (coverByte << nrOfBits);
+            coverByte = (byte) (coverByte | stegoByte);
+            cover.setByte(nextIndex, coverByte);
+        }
+        if (stego.hasMoreBits()) {
+            System.out.println("Couldn't encode the whole message, container was too small");
+        }
+    }
+
+    public void decode(IContainerFile cover, IWriteStegoFile stego) {
+        System.out.println("decoding...");
+
+        RandomUnique randGen = new RandomUnique(seed, cover.getSize());
+        Set<Integer> indexValues = randGen.nextInt(stego.getSize() / nrOfBits);
+        Iterator<Integer> it = indexValues.iterator();
+        while (it.hasNext()) {
+            int nextIndex = it.next();
+
+            byte coverByte = cover.getByte(nextIndex);
+            short mask = ByteHelper.getMask(nrOfBits - 1, nrOfBits);
+            byte stegoByte = (byte) (coverByte & mask);
+            stego.setNextBits(stegoByte, nrOfBits);
+        }
+        if (stego.hasMoreBits()) {
+            System.out.println("Couldn't decode the whole message, container was too small");
+        }
+    }
 }
