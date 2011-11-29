e886613e52efa4215631986bfc48803af904fdb4
julio.var@gmail.com <julio.var@gmail.com@4297f225-f6bb-08c7-5789-f87774b56954>
2011-11-29 14:50:06 +0000

    controller and view classes

---
+++ trunk/ID2212/HW4/src/java/converter/controller/CurrencyFacade.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package converter.controller;
+
+import javax.ejb.Stateless;
+import javax.ejb.LocalBean;
+
+/**
+ *
+ * @author julio
+ */
+@Stateless
+@LocalBean
+public class CurrencyFacade {
+
+    
+    
+}
