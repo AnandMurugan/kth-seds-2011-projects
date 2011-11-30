664191cfaf964f8c8b20f35b506f4c32cf51d126
julio.var@gmail.com <julio.var@gmail.com@4297f225-f6bb-08c7-5789-f87774b56954>
2011-11-30 14:48:33 +0000

    ad

--- ID2212/HW4/src/java/converter/controller/ConverterFacade.java
+++ ID2212/HW4/src/java/converter/controller/ConverterFacade.java
@@ -4,10 +4,13 @@
  */
 package converter.controller;
 
+import converter.model.Currency;
 import converter.model.CurrencyDTO;
 import java.util.List;
 import javax.ejb.Stateless;
 import javax.ejb.LocalBean;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 /**
  *
@@ -16,11 +19,27 @@ import javax.ejb.LocalBean;
 @Stateless
 @LocalBean
 public class ConverterFacade {
-
     public List<CurrencyDTO> getCurrencyList() {
         throw new UnsupportedOperationException("Not yet implemented");
     }
+    @PersistenceContext(unitName = "currencyPU")
+    private EntityManager em;
 
-    
-    
+    public CurrencyDTO createCurrency(String symbol, String country, String name) {
+        Currency newCurrency = new Currency(symbol, country, name);
+        em.persist(newCurrency);
+        return newCurrency;
+    }
+
+    public boolean login(String userId, String pwd) {
+        boolean valid = false;
+        if (userId.equals("test") && pwd.equals("test")) {
+            valid = true;
+        }
+
+        /*if (found == null) {
+        throw new EntityNotFoundException("No account with number " + acctNo);
+        }*/
+        return valid;
+    }
 }
