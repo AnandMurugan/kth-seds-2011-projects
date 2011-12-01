442c3c8a63dbad1bd63aaf728352b65cf56a93c9
julio.var@gmail.com <julio.var@gmail.com@4297f225-f6bb-08c7-5789-f87774b56954>
2011-12-01 13:02:44 +0000

    added ConverterUser entity

---
+++ ID2212/HW4/src/java/converter/model/ConverterUserDTO.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package converter.model;
+
+/**
+ *
+ * @author julio
+ */
+public interface ConverterUserDTO {
+    String getUserName();
+}
