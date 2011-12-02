765f9ab4582dcb1424b9d1773188406f6eb579ee
igor.kostyakov@gmail.com <igor.kostyakov@gmail.com@4297f225-f6bb-08c7-5789-f87774b56954>
2011-12-02 10:43:53 +0000

    ID2212_Project

---
+++ ID2212/Project/src/fish/common/RejectedException.java
@@ -0,0 +1,12 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fish.common;
+
+/**
+ *
+ * @author Igor
+ */
+public class RejectedException extends Exception {
+}
