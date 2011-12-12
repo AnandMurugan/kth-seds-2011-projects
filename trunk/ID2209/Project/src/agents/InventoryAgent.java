60a91d283f224116e7a0fd46b4df00111e89d6b6
igor.kostyakov@gmail.com <igor.kostyakov@gmail.com@4297f225-f6bb-08c7-5789-f87774b56954>
2011-12-12 15:45:39 +0000

    ID2209_Project: item reader

--- trunk/ID2209/Project/src/agents/InventoryAgent.java
+++ trunk/ID2209/Project/src/agents/InventoryAgent.java
@@ -4,10 +4,82 @@
  */
 package agents;
 
+import items.MuseumItem;
+import jade.core.Agent;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Igor
  */
-public class InventoryAgent {
-    
+public class InventoryAgent extends Agent {
+    private static final String DELIMETERS = ",";
+    private Map<String, MuseumItem> items;
+
+    @Override
+    protected void setup() {
+        try {
+            initItems();
+        } catch (Exception ex) {
+            Logger.getLogger(InventoryAgent.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    private void initItems() throws FileNotFoundException, IOException {
+        items = new HashMap<String, MuseumItem>();
+        File itemsFile = new File("data/items.csv");
+
+        BufferedReader br = new BufferedReader(new FileReader(itemsFile));
+
+        String line;
+        while ((line = br.readLine()) != null) {
+            StringTokenizer st = new StringTokenizer(line, DELIMETERS);
+
+            String id = st.nextToken();
+            String title = st.nextToken();
+            StringTokenizer st2 = new StringTokenizer(st.nextToken(), ";");
+            String[] subject = new String[st2.countTokens()];
+            for (int i = 0; i < st2.countTokens(); i++) {
+                subject[i] = st2.nextToken();
+            }
+            String objectType1 = st.nextToken();
+            String objectType2 = st.nextToken();
+            String objectType3 = st.nextToken();
+            String[] objectType = new String[]{objectType1, objectType2, objectType3};
+            String material1 = st.nextToken();
+            String material2 = st.nextToken();
+            String material3 = st.nextToken();
+            String material4 = st.nextToken();
+            String material5 = st.nextToken();
+            String[] material = new String[]{material1, material2, material3, material4, material5};
+
+            items.put(id, new MuseumItem(id, title, subject, objectType, material));
+        }
+
+        br.close();
+    }
+
+    private MuseumItem getItem(String id) {
+        return items.get(id);
+    }
+//    public static void main(String[] args) throws IOException {
+//        InventoryAgent a = new InventoryAgent();
+//        a.setup();
+//
+//        while (true) {
+//            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//            String id = br.readLine();
+//
+//            System.out.println(a.getItem(id));
+//        }
+//    }
 }
