0d00e9eee410f98091b096f7794026ac67424a6a
igor.kostyakov@gmail.com <igor.kostyakov@gmail.com@4297f225-f6bb-08c7-5789-f87774b56954>
2011-12-12 14:39:59 +0000

    ID2209_Project: items

--- trunk/ID2209/Project/src/items/MuseumItem.java
+++ trunk/ID2209/Project/src/items/MuseumItem.java
@@ -8,6 +8,27 @@ package items;
  *
  * @author Igor
  */
-public class MuseumItem {
-    
+public class MuseumItem extends TourItem {
+    private String[] subject;
+    private String[] objectType;
+    private String[] material;
+
+    public MuseumItem(String[] subject, String[] objectType, String[] material, String id, String name) {
+        super(id, name);
+        this.subject = subject;
+        this.objectType = objectType;
+        this.material = material;
+    }
+
+    public String[] getMaterial() {
+        return material;
+    }
+
+    public String[] getObjectType() {
+        return objectType;
+    }
+
+    public String[] getSubject() {
+        return subject;
+    }
 }
--- trunk/ID2209/Project/src/items/TourItem.java
+++ trunk/ID2209/Project/src/items/TourItem.java
@@ -9,5 +9,19 @@ package items;
  * @author Igor
  */
 public class TourItem {
-    
+    private String id;
+    private String name;
+
+    public TourItem(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
