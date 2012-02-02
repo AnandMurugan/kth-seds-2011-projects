f5c981aabb1a4890d6404d08303f2682c0e1863f
julio.var@gmail.com <julio.var@gmail.com@4297f225-f6bb-08c7-5789-f87774b56954>
2012-02-02 13:54:32 +0000

    updates

--- ID2208/HW1/src/mypack/ApplicantProfileXmlModel.java
+++ ID2208/HW1/src/mypack/ApplicantProfileXmlModel.java
@@ -32,16 +32,123 @@ public class ApplicantProfileXmlModel {
         profileXmlDoc = DOMParser.getDOMDocument(this.schemaLanguage, this.schemaPath, this.xmlFilePath);
     }
 
-    public void setCompanyInfo(String companyName, String description, String email, String website) {
-        // check if there is already a node created with the company info
+    public void setCompanyInfo(String companyName, String description, String email, String website, String phone, String businessType) {
         NodeList nodes = profileXmlDoc.getElementsByTagName("ns1:company");
 
-        if (nodes.getLength() > 0) {
-            System.out.println("Matched!!");
-        } else {
+        if (nodes.getLength() > 0) // if there are company info nodes
+        {
+            String currentCompanyName;
+            for (int i = 0; i < nodes.getLength(); i++) {
+                currentCompanyName = nodes.item(i).getFirstChild().getFirstChild().getTextContent();
+                if (currentCompanyName.equals(companyName)) {
+
+                    Node companyProfileNode = DOMParser.findNode(nodes.item(i).getChildNodes(), "ns1:company_profile");
+                    if (companyProfileNode != null) {
+                        // set description
+                        Node descriptionNode = DOMParser.findNode(companyProfileNode.getChildNodes(),"");
+                        descriptionNode.setTextContent(description);
+                        // set phone
+                        Node phoneNode = DOMParser.findNode(companyProfileNode.getChildNodes(),"");
+                        phoneNode.setTextContent(phone);
+                        // set email
+                        Node emailNode = DOMParser.findNode(companyProfileNode.getChildNodes(),"");
+                        emailNode.setTextContent(email);
+                        // set website
+                        Node websiteNode = DOMParser.findNode(companyProfileNode.getChildNodes(),"");
+                        websiteNode.setTextContent(website);
+                        // set business type
+                        Node businessTypeNode = DOMParser.findNode(companyProfileNode.getChildNodes(),"");
+                        businessTypeNode.setTextContent(businessType);
+                    }
+                }
+            }
         }
     }
 
+    public void setDescription(String companyName, String description){
+        NodeList nodes = profileXmlDoc.getElementsByTagName("ns1:company");
+
+        if (nodes.getLength() > 0) // if there are company info nodes
+        {
+            String currentCompanyName;
+            for (int i = 0; i < nodes.getLength(); i++) {
+                currentCompanyName = nodes.item(i).getFirstChild().getFirstChild().getTextContent();
+                if (currentCompanyName.equals(companyName)) {
+
+                    Node companyProfileNode = DOMParser.findNode(nodes.item(i).getChildNodes(), "ns1:company_profile");
+                    if (companyProfileNode != null) {
+                        // set description
+                        Node descriptionNode = DOMParser.findNode(companyProfileNode.getChildNodes(),"ns1:description");
+                        descriptionNode.setTextContent(description);
+                    }
+                }
+            }
+        }
+    }
+    
+    public void setPhone(String companyName, String phone){
+        NodeList nodes = profileXmlDoc.getElementsByTagName("ns1:company");
+
+        if (nodes.getLength() > 0) // if there are company info nodes
+        {
+            String currentCompanyName;
+            for (int i = 0; i < nodes.getLength(); i++) {
+                currentCompanyName = nodes.item(i).getFirstChild().getFirstChild().getTextContent();
+                if (currentCompanyName.equals(companyName)) {
+
+                    Node companyProfileNode = DOMParser.findNode(nodes.item(i).getChildNodes(), "ns1:company_profile");
+                    if (companyProfileNode != null) {
+                        // set phone
+                        Node phoneNode = DOMParser.findNode(companyProfileNode.getChildNodes(),"ns1:phone");
+                        phoneNode.setTextContent(phone);
+                    }
+                }
+            }
+        }
+    }
+    
+    public void setWebsite(String companyName, String website){
+        NodeList nodes = profileXmlDoc.getElementsByTagName("ns1:company");
+
+        if (nodes.getLength() > 0) // if there are company info nodes
+        {
+            String currentCompanyName;
+            for (int i = 0; i < nodes.getLength(); i++) {
+                currentCompanyName = nodes.item(i).getFirstChild().getFirstChild().getTextContent();
+                if (currentCompanyName.equals(companyName)) {
+
+                    Node companyProfileNode = DOMParser.findNode(nodes.item(i).getChildNodes(), "ns1:company_profile");
+                    if (companyProfileNode != null) {
+                        // set website
+                        Node websiteNode = DOMParser.findNode(companyProfileNode.getChildNodes(),"ns1:website");
+                        websiteNode.setTextContent(website);
+                    }
+                }
+            }
+        }
+    }
+    
+    public void setEmail(String companyName, String email){
+        NodeList nodes = profileXmlDoc.getElementsByTagName("ns1:company");
+
+        if (nodes.getLength() > 0) // if there are company info nodes
+        {
+            String currentCompanyName;
+            for (int i = 0; i < nodes.getLength(); i++) {
+                currentCompanyName = nodes.item(i).getFirstChild().getFirstChild().getTextContent();
+                if (currentCompanyName.equals(companyName)) {
+
+                    Node companyProfileNode = DOMParser.findNode(nodes.item(i).getChildNodes(), "ns1:company_profile");
+                    if (companyProfileNode != null) {
+                        // set email
+                        Node emailNode = DOMParser.findNode(companyProfileNode.getChildNodes(),"ns1:email");
+                        emailNode.setTextContent(email);
+                    }
+                }
+            }
+        }
+    }
+    
     public void setBusinessType(String companyName, String businessType) {
         NodeList nodes = profileXmlDoc.getElementsByTagName("ns1:company");
 
@@ -52,15 +159,17 @@ public class ApplicantProfileXmlModel {
                 currentCompanyName = nodes.item(i).getFirstChild().getFirstChild().getTextContent();
                 if (currentCompanyName.equals(companyName)) {
 
-                    Node businessTypeNode = DOMParser.findNode(nodes.item(i).getChildNodes(), "ns1:businessType");
-                    if (businessTypeNode != null) {
+                    Node companyProfileNode = DOMParser.findNode(nodes.item(i).getChildNodes(), "ns1:company_profile");
+                    if (companyProfileNode != null) {
+                        // set business type
+                        Node businessTypeNode = DOMParser.findNode(companyProfileNode.getChildNodes(),"ns1:businessType");
                         businessTypeNode.setTextContent(businessType);
                     }
                 }
             }
         }
     }
-
+    
     
     
     public void setEmploymentRecord(String companyName, Node positions) {
