6ce178b1d50c9aec84d08e3f5f62587d7811b995
julio.var@gmail.com <julio.var@gmail.com@4297f225-f6bb-08c7-5789-f87774b56954>
2012-02-02 13:04:12 +0000

    updates

--- ID2208/HW1/src/mypack/ApplicantProfileGenerator.java
+++ ID2208/HW1/src/mypack/ApplicantProfileGenerator.java
@@ -4,42 +4,86 @@
  */
 package mypack;
 
+import java.io.File;
+import java.io.IOException;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParserFactory;
+import mypack.SAXParser.CompanyInfoParser;
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
 /**
  *
  * @author julio
  */
 public class ApplicantProfileGenerator {
-    String applicantProfileSchemaPath = "applicantProfileXmlSchema.xsd";
-    String companyInfoSchemaPath = "companyInfoXmlSchema.xsd";
-    String employmentRecordSchemaPath = "employmentRecordXmlSchema.xsd";
-    String resumeSchemaPath = "resume.xsd";
-    String transcriptSchemaPath = "transcriptSchema.xsd";
+    String profileSchemaPath = "src/schemas/applicantProfileXmlSchema.xsd";
+    String companyInfoSchemaPath = "src/schemas/companyInfoXmlSchema.xsd";
+    String employmentRecordSchemaPath = "src/schemas/employmentRecordXmlSchema.xsd";
+    String resumeSchemaPath = "src/schemas/resume.xsd";
+    String transcriptSchemaPath = "src/schemas/transcriptSchema.xsd";
+    static final String JAXP_SCHEMA_SOURCE = "http://java.sun.com/xml/jaxp/properties/schemaSource";
+    static final String SCHEMA_LANGUAGE = "http://java.sun.com/xml/jaxp/properties/schemaLanguage";
     
-    public ApplicantProfileGenerator(){
-    
+    public ApplicantProfileGenerator() {
     }
-    
+
     public ApplicantProfileGenerator(String applicantProfileSchemaPath,
             String companyInfoSchemaPath,
             String employmentRecordSchemaPath,
             String resumeSchemaPath,
-            String transcriptSchemaPath){
-        this.applicantProfileSchemaPath = applicantProfileSchemaPath;
+            String transcriptSchemaPath) {
+        this.profileSchemaPath = applicantProfileSchemaPath;
         this.companyInfoSchemaPath = companyInfoSchemaPath;
         this.employmentRecordSchemaPath = employmentRecordSchemaPath;
         this.resumeSchemaPath = resumeSchemaPath;
         this.transcriptSchemaPath = transcriptSchemaPath;
     }
-    
+
     // TODO. define the return type of the generated application profile
-    public void Generate(String companiesXmlPath, String employmentRecordsPath, String resumeXmlPath, String transcriptXmlPath){
-        // Call each parser to process the correspondant file
+    public void Generate(String companiesXmlPath, 
+            String employmentRecordXmlPath, 
+            String resumeXmlPath, 
+            String transcriptXmlPath, 
+            String profileXmlPath) {
+        try {
+            // Create Application Profile Model
+            ApplicantProfileXmlModel profileModel = new ApplicantProfileXmlModel(this.profileSchemaPath, profileXmlPath);
+                        
+            //SAX Parsing
+            // Create Parser
+            SAXParserFactory saxpf = SAXParserFactory.newInstance();
+            saxpf.setNamespaceAware(true);
+            saxpf.setValidating(true);
+            javax.xml.parsers.SAXParser saxp = saxpf.newSAXParser();
+            //saxp.setProperty("http://xml.org/sax/features/validation", true);
+            // Ensure namespace processing is on (the default)
+            //saxp.setProperty("http://xml.org/sax/features/namespaces", true);
+            saxp.setProperty("http://java.sun.com/xml/jaxp/properties/schemaLanguage",
+                    "http://www.w3.org/2001/XMLSchema");
+            //specifies the XML schema document to be used for validation.
+            saxp.setProperty(JAXP_SCHEMA_SOURCE, new File("src/schemas/companyInfoXmlSchema.xsd"));
+            saxp.parse("src/xml/companiesDatabase.xml", new CompanyInfoParser(profileModel));
+            profileModel.overriteXmlFile();
+            
+            
+            
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } catch (ParserConfigurationException ex) {
+            ex.printStackTrace();
+        } catch (SAXException ex) {
+            ex.printStackTrace();
+        }
+
+
+
     }
-    
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         // TODO. get the input xml files from the command line params
         ApplicantProfileGenerator generator = new ApplicantProfileGenerator();
-        generator.Generate(null, null, null, null);
-    
+        generator.Generate("src/xml/companiesDatabase.xml", "", "", "", "src/xml/applicantProfile.xml");
+
     }
 }
--- ID2208/HW1/src/mypack/DOMParser.java
+++ ID2208/HW1/src/mypack/DOMParser.java
@@ -1,175 +1,213 @@
 package mypack;
 
 import java.io.File;
+import java.io.IOException;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
-
+import org.xml.sax.SAXException;
 
 public class DOMParser {
+    String schemaPath;
+    String schemaLanguage;
 
-	
-	public static void main(String[] args) {
-		try{
-			
-		      //Get a factory object for DocumentBuilder objects
-		      DocumentBuilderFactory factory =
-		            DocumentBuilderFactory.newInstance();
+    public DOMParser() {
+    }
 
-		      
-		      // to make the parser a validating parse
-		      factory.setValidating(true);
-		      //To parse a XML document with a namespace,
-		      factory.setNamespaceAware(true);
-		      
-		      // to ignore cosmetic whitespace between elements.
-		      factory.setIgnoringElementContentWhitespace(true);
-		      factory.setAttribute(
-		    		    "http://java.sun.com/xml/jaxp/properties/schemaLanguage",
-		    		    "http://www.w3.org/2001/XMLSchema");
-		      //specifies the XML schema document to be used for validation.
-		      factory.setAttribute(
-		    		    "http://java.sun.com/xml/jaxp/properties/schemaSource",
-		    		    "Transcript.xsd");
-		    	
-		      //Get a DocumentBuilder (parser) object
-		      	      
-		      DocumentBuilder builder =
-		                    factory.newDocumentBuilder();
+    public DOMParser(String aSchemaPath, String aSchemaLanguage) {
+        this.schemaLanguage = aSchemaLanguage;
+        this.schemaPath = aSchemaPath;
+    }
 
-		      //Parse the XML input file to create a
-		      // Document object that represents the
-		      // input XML file.
-		      ///
-		      Document document = builder.parse(
-		                              new File("xml/Transcript.xml"));
+    public static Document getDOMDocument(String schemaLanguage, String schemaFilePath, String aXmlFilePath) {
+        Document document = null;
+        try {
+            DocumentBuilderFactory factory =
+                    DocumentBuilderFactory.newInstance();
 
-		      //Instantiate an object of this class
-		      ///
-		      DOMParser domParser = new DOMParser();
+            // to make the parser a validating parse
+            factory.setValidating(true);
+            //To parse a XML document with a namespace,
+            factory.setNamespaceAware(true);
 
-		       //Process the DOM tree, beginning with the
-		      // Document node to produce the output.
-		      // Invocation of processDocumentNode starts
-		      // a recursive process that processes the
-		      // entire DOM tree.
-		      ///                      
-             // domParser.processNode(document);
-              // OR 
-              domParser.processTranscript(document.getFirstChild());
-		    
-		}catch(Exception e){
-		      
-		      e.printStackTrace(System.err);
-		    }//end catch
-	}
-	
-	Node findNode(NodeList nodes, String nodeName)
-	 {
-		 for(int i=0; i<nodes.getLength(); i++)
-		 {
-			 if(nodes.item(i).getNodeName().equals(nodeName))
-			 {
-				 return nodes.item(i);
-			 }
-		 }
-		 return null;
-	 }
-	 
-	 String getNodeValue(NodeList nodes, String nodeName)
-	 {
-		 Node result = findNode(nodes, nodeName);
-		 if(result != null)
-		 {
-			 return result.getFirstChild().getNodeValue();
-		 }
-		 return null;
-	 }
-		
-	
-	 void processNode(Node root)
-	 {
+            // to ignore cosmetic whitespace between elements.
+            factory.setIgnoringElementContentWhitespace(true);
+            factory.setAttribute(
+                    "http://java.sun.com/xml/jaxp/properties/schemaLanguage",
+                    schemaLanguage);
+            //specifies the XML schema document to be used for validation.
+            factory.setAttribute(
+                    "http://java.sun.com/xml/jaxp/properties/schemaSource",
+                    schemaFilePath);
 
-	    try{
-	      if (root == null){
-	        System.err.println("Node is null !!!");
-	        return;
-	      }
+            //Get a DocumentBuilder (parser) object
 
-	      int type = root.getNodeType();
-	      
-	      switch (type){
-	        case Node.TEXT_NODE:{
-	            System.out.print("Text Node: <" +  root.getNodeValue() +  ">\n");
-	          
-	          break;
-	        }//end case Node.TEXT_NODE
+            DocumentBuilder builder =
+                    factory.newDocumentBuilder();
 
-	        case Node.ATTRIBUTE_NODE:{
-	        	System.out.print("Attribute Node: <" + root.getNodeName() + " = " +  root.getNodeValue() + " >\n");
-	        	break;
-	        }
+            //Parse the XML input file to create a
+            // Document object that represents the
+            // input XML file.
+            ///
+            document = builder.parse(
+                    new File(aXmlFilePath));
+        } catch (SAXException ex) {
+            ex.printStackTrace();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } catch (ParserConfigurationException ex) {
+            ex.printStackTrace();
+        }
+        return document;
+    }
 
-	        case Node.ELEMENT_NODE:{
-	        	NodeList children = root.getChildNodes();
-	        	System.out.print("Element Node: <" + root.getNodeName() +"> has "+ children.getLength()+" child elements.\n");	        		          
-	            
+    public static void main(String[] args) {
+        try {
+            //Get a factory object for DocumentBuilder objects
+            DocumentBuilderFactory factory =
+                    DocumentBuilderFactory.newInstance();
 
-	            for (Node child = root.getFirstChild();   child != null;   child = child.getNextSibling())
-	            	processNode(child);
-	          break;
-	        }//end case ELEMENT_NODE
 
-	        case Node.DOCUMENT_NODE:{
-	        	NodeList children = root.getChildNodes();
-	        	System.out.print("Document Node: <" + root.getNodeName() + "> has "+children.getLength()+" child elements.\n");	        		 
-	            
-	            for (Node child = root.getFirstChild();   child != null;   child = child.getNextSibling())
-	            	processNode(child);
-                        
-	          break;
-	        }//end case DOCUMENT_NODE
+            // to make the parser a validating parse
+            factory.setValidating(true);
+            //To parse a XML document with a namespace,
+            factory.setNamespaceAware(true);
 
-	        // there are even more cases to check
-	        default:{
+            // to ignore cosmetic whitespace between elements.
+            factory.setIgnoringElementContentWhitespace(true);
+            factory.setAttribute(
+                    "http://java.sun.com/xml/jaxp/properties/schemaLanguage",
+                    "http://www.w3.org/2001/XMLSchema");
+            //specifies the XML schema document to be used for validation.
+            factory.setAttribute(
+                    "http://java.sun.com/xml/jaxp/properties/schemaSource",
+                    "Transcript.xsd");
 
-	        }//end default
+            //Get a DocumentBuilder (parser) object
 
-	      }//end switch
+            DocumentBuilder builder =
+                    factory.newDocumentBuilder();
 
-	    }catch(Exception e){
-	      e.printStackTrace(System.err);
-	    }//end catch
-  }
+            //Parse the XML input file to create a
+            // Document object that represents the
+            // input XML file.
+            ///
+            Document document = builder.parse(
+                    new File("xml/Transcript.xml"));
 
-	 
-	 
-	 void processTranscript(Node root)
-	 {
-		 try
-		 {			 
-			 if(root.getNodeName().equals("transcript"))
-			 {				 
-				 							
-				 String student_name = getNodeValue(root.getChildNodes(), "name");							 							 
-				 String univ_name= getNodeValue(root.getChildNodes(), "university");
-				 String degree = getNodeValue(root.getChildNodes(), "degree");							 							 
-				 String year= getNodeValue(root.getChildNodes(), "year");
-				 NodeList courseList = root.getChildNodes();
-				 String[] courses = new String[courseList.getLength()];
-				 
-				 for(int i=0; i<courseList.getLength(); i++)
-				 {
-					 courses[i] = courseList.item(0).getNodeValue();
-				 }
-				 System.out.print(student_name + ", " + univ_name + " , " + degree + ", " + year + " ,");
-			 }						 
-		 }
-		 catch(Exception e)
-		 {
-			 e.printStackTrace(System.err);
-	     }
-	 }
+            //Instantiate an object of this class
+            ///
+            DOMParser domParser = new DOMParser();
+
+            //Process the DOM tree, beginning with the
+            // Document node to produce the output.
+            // Invocation of processDocumentNode starts
+            // a recursive process that processes the
+            // entire DOM tree.
+            ///                      
+            // domParser.processNode(document);
+            // OR 
+            domParser.processTranscript(document.getFirstChild());
+
+        } catch (Exception e) {
+
+            e.printStackTrace(System.err);
+        }//end catch
+    }
+
+    public static Node findNode(NodeList nodes, String nodeName) {
+        for (int i = 0; i < nodes.getLength(); i++) {
+            if (nodes.item(i).getNodeName().equals(nodeName)) {
+                return nodes.item(i);
+            }
+        }
+        return null;
+    }
+
+    String getNodeValue(NodeList nodes, String nodeName) {
+        Node result = findNode(nodes, nodeName);
+        if (result != null) {
+            return result.getFirstChild().getNodeValue();
+        }
+        return null;
+    }
+
+    void processNode(Node root) {
+
+        try {
+            if (root == null) {
+                System.err.println("Node is null !!!");
+                return;
+            }
+
+            int type = root.getNodeType();
+
+            switch (type) {
+                case Node.TEXT_NODE: {
+                    System.out.print("Text Node: <" + root.getNodeValue() + ">\n");
+
+                    break;
+                }//end case Node.TEXT_NODE
+
+                case Node.ATTRIBUTE_NODE: {
+                    System.out.print("Attribute Node: <" + root.getNodeName() + " = " + root.getNodeValue() + " >\n");
+                    break;
+                }
+
+                case Node.ELEMENT_NODE: {
+                    NodeList children = root.getChildNodes();
+                    System.out.print("Element Node: <" + root.getNodeName() + "> has " + children.getLength() + " child elements.\n");
+
+
+                    for (Node child = root.getFirstChild(); child != null; child = child.getNextSibling()) {
+                        processNode(child);
+                    }
+                    break;
+                }//end case ELEMENT_NODE
+
+                case Node.DOCUMENT_NODE: {
+                    NodeList children = root.getChildNodes();
+                    System.out.print("Document Node: <" + root.getNodeName() + "> has " + children.getLength() + " child elements.\n");
+
+                    for (Node child = root.getFirstChild(); child != null; child = child.getNextSibling()) {
+                        processNode(child);
+                    }
+
+                    break;
+                }//end case DOCUMENT_NODE
+
+                // there are even more cases to check
+                default: {
+                }//end default
+
+            }//end switch
+
+        } catch (Exception e) {
+            e.printStackTrace(System.err);
+        }//end catch
+    }
+
+    void processTranscript(Node root) {
+        try {
+            if (root.getNodeName().equals("transcript")) {
+
+                String student_name = getNodeValue(root.getChildNodes(), "name");
+                String univ_name = getNodeValue(root.getChildNodes(), "university");
+                String degree = getNodeValue(root.getChildNodes(), "degree");
+                String year = getNodeValue(root.getChildNodes(), "year");
+                NodeList courseList = root.getChildNodes();
+                String[] courses = new String[courseList.getLength()];
+
+                for (int i = 0; i < courseList.getLength(); i++) {
+                    courses[i] = courseList.item(0).getNodeValue();
+                }
+                System.out.print(student_name + ", " + univ_name + " , " + degree + ", " + year + " ,");
+            }
+        } catch (Exception e) {
+            e.printStackTrace(System.err);
+        }
+    }
 }
