d7ae6ad6e01b49177a24af850e1d070f44a83058
igor.kostyakov@gmail.com <igor.kostyakov@gmail.com@4297f225-f6bb-08c7-5789-f87774b56954>
2012-02-07 18:10:18 +0000

    fixed package names

---
+++ trunk/ID2203/HW1/src/se/kth/ict/id2203/epfd/EventuallyPerfectFailureDetector.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package se.kth.ict.id2203.epfd;
+
+import se.sics.kompics.PortType;
+
+/**
+ *
+ * @author Igor
+ */
+public class EventuallyPerfectFailureDetector extends PortType {
+
+    {
+        indication(Suspect.class);
+        indication(Restore.class);
+    }
+}
