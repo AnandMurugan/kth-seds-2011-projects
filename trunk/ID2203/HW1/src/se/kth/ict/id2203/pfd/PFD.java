5839e1e2cec144e3ea1962a9fb944d84f31c9280
julio.var@gmail.com <julio.var@gmail.com@4297f225-f6bb-08c7-5789-f87774b56954>
2012-02-07 22:16:52 +0000

    added some changes

--- trunk/ID2203/HW1/src/se/kth/ict/id2203/pfd/PFD.java
+++ trunk/ID2203/HW1/src/se/kth/ict/id2203/pfd/PFD.java
@@ -6,7 +6,9 @@ package se.kth.ict.id2203.pfd;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 import se.kth.ict.id2203.pp2p.PerfectPointToPointLink;
+import se.kth.ict.id2203.pp2p.Pp2pSend;
 import se.sics.kompics.ComponentDefinition;
 import se.sics.kompics.Handler;
 import se.sics.kompics.Negative;
@@ -23,23 +25,31 @@ public final class PFD extends ComponentDefinition {
     Negative<PerfectFailureDetector> pfd = provides(PerfectFailureDetector.class);
     Positive<PerfectPointToPointLink> pp2p = requires(PerfectPointToPointLink.class);
     Positive<Timer> timer = requires(Timer.class);
-    private List<Address> neighborNodes;
+    private Set<Address> neighborNodes;
     private List<Address> aliveNodes;
     private List<Address> detectedNodes;
-    private int heartbeatInterval = 1000;
-    private int checkInterval = 5000;
+    private int heartbeatInterval;
+    private int checkInterval;
+    private Address self;
 
     public PFD() {
         subscribe(handleInit, control);
         subscribe(handleHeartbeatTimeout, timer);
         subscribe(handleCheckTimeout, timer);
+        subscribe(handleHeartbeatDeliver, pfd);
+        detectedNodes = new ArrayList<Address>();
     }
     Handler<PfdInit> handleInit = new Handler<PfdInit>() {
         @Override
         public void handle(PfdInit e) {
-            //  aliveNodes = neighborNodes;
-            detectedNodes = new ArrayList<Address>();
-            aliveNodes = neighborNodes;
+            neighborNodes = e.getNeighborSet();
+            self = e.getSelf();
+            heartbeatInterval = e.getHeartbeatInterval();
+            checkInterval = e.getCheckInterval();
+
+            for (Address addr : neighborNodes) {
+                aliveNodes.add(addr);
+            }
 
             ScheduleTimeout hbt = new ScheduleTimeout(heartbeatInterval);
             hbt.setTimeoutEvent(new HeartbeatTimeout(hbt));
@@ -50,17 +60,38 @@ public final class PFD extends ComponentDefinition {
             trigger(ct, timer);
         }
     };
-    
     Handler<HeartbeatTimeout> handleHeartbeatTimeout = new Handler<HeartbeatTimeout>() {
         @Override
         public void handle(HeartbeatTimeout e) {
-            throw new UnsupportedOperationException("Not supported yet.");
+            for (Address addr : neighborNodes) {
+                trigger(new Pp2pSend(addr, new HeartbeatMessage(self)), pp2p);
+            }
+
+            ScheduleTimeout hbt = new ScheduleTimeout(heartbeatInterval);
+            hbt.setTimeoutEvent(new HeartbeatTimeout(hbt));
+            trigger(hbt, timer);
         }
     };
     Handler<CheckTimeout> handleCheckTimeout = new Handler<CheckTimeout>() {
         @Override
         public void handle(CheckTimeout e) {
-            throw new UnsupportedOperationException("Not supported yet.");
+            for (Address addr : neighborNodes) {
+                if (!aliveNodes.contains(addr) && (!detectedNodes.contains(addr))) {
+                    detectedNodes.add(addr);
+                    trigger(new PfdCrash(addr), pfd);
+                }
+            }
+
+            aliveNodes.clear();
+            ScheduleTimeout ct = new ScheduleTimeout(heartbeatInterval + checkInterval);
+            ct.setTimeoutEvent(new HeartbeatTimeout(ct));
+            trigger(ct, timer);
+        }
+    };
+    Handler<HeartbeatMessage> handleHeartbeatDeliver = new Handler<HeartbeatMessage>() {
+        @Override
+        public void handle(HeartbeatMessage e) {
+            aliveNodes.add(e.getSource());
         }
     };
 }
--- trunk/ID2203/HW1/src/se/kth/ict/id2203/pfd/PfdInit.java
+++ trunk/ID2203/HW1/src/se/kth/ict/id2203/pfd/PfdInit.java
@@ -4,12 +4,50 @@
  */
 package se.kth.ict.id2203.pfd;
 
+import java.util.Set;
 import se.sics.kompics.Init;
+import se.sics.kompics.address.Address;
 
 /**
  *
  * @author julio
  */
 public final class PfdInit extends Init {
+    private final Set<Address> neighborSet;
+    private final Address self;
+    private final int heartbeatInterval;
+    private final int checkInterval;
+
+    public PfdInit(Set<Address> neighborSet, Address self, int heartbeatInterval, int checkInterval) {
+        this.neighborSet = neighborSet;
+        this.self = self;
+        this.heartbeatInterval = heartbeatInterval;
+        this.checkInterval = checkInterval;
+    }
+
+    /**
+     * Gets the neighbor set.
+     * 
+     * @return the neighbor set
+     */
+    public final Set<Address> getNeighborSet() {
+        return neighborSet;
+    }
+
+    /**
+     * Gets the self.
+     * 
+     * @return the self
+     */
+    public final Address getSelf() {
+        return self;
+    }
     
+    public final int getHeartbeatInterval() {
+        return heartbeatInterval;
+    }
+
+    public final int getCheckInterval() {
+        return checkInterval;
+    }
 }
