84e7cc26ad993fd6eb9c35b8a04e6e43a5113e63
igor.kostyakov@gmail.com <igor.kostyakov@gmail.com@4297f225-f6bb-08c7-5789-f87774b56954>
2012-02-21 10:31:38 +0000

    hw3 classes

--- trunk/ID2203/HW1/src/se/kth/ict/id2203/registers/atomic/AtomicRegister.java
+++ trunk/ID2203/HW1/src/se/kth/ict/id2203/registers/atomic/AtomicRegister.java
@@ -4,10 +4,17 @@
  */
 package se.kth.ict.id2203.registers.atomic;
 
+import se.sics.kompics.PortType;
+
 /**
  *
  * @author Igor
  */
-public class AtomicRegister {
-    
+public class AtomicRegister extends PortType {
+    {
+        indication(ReadResponse.class);
+        indication(WriteResponse.class);
+        request(ReadRequest.class);
+        request(WriteRequest.class);
+    }
 }
