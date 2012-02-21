13ba8ea03059309b31d0fd1bda2b2c0ca4c3a63f
igor.kostyakov@gmail.com <igor.kostyakov@gmail.com@4297f225-f6bb-08c7-5789-f87774b56954>
2012-02-21 15:50:16 +0000

    riwcm

---
+++ ID2203/HW1/src/se/kth/ict/id2203/registers/atomic/AckMessage.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package se.kth.ict.id2203.registers.atomic;
+
+import se.kth.ict.id2203.pp2p.Pp2pDeliver;
+import se.sics.kompics.address.Address;
+
+/**
+ *
+ * @author Igor
+ */
+public class AckMessage extends Pp2pDeliver {
+    public AckMessage(Address source) {
+        super(source);
+    }
+}
