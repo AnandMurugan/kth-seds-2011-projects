76c3351883527f2a5934bdde6109aec13f9311b4
julio.var@gmail.com <julio.var@gmail.com@4297f225-f6bb-08c7-5789-f87774b56954>
2012-02-22 02:22:58 +0000

    upload files

---
+++ trunk/ID2203/HW1/src/se/kth/ict/id2203/Assignment3aExecutor.java
@@ -0,0 +1,98 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package se.kth.ict.id2203;
+
+import se.sics.kompics.launch.Scenario;
+import se.sics.kompics.launch.Topology;
+
+/**
+ *
+ * @author julio
+ */
+public class Assignment3aExecutor {
+    private static final int NODES = 3;
+
+    public static void main(String[] args) {
+        Topology topology1 = new Topology() {
+            {
+                node(1, "127.0.0.1", 10001);
+                node(2, "127.0.0.1", 10002);
+                node(3, "127.0.0.1", 10003);
+
+                //link(1, 2, 500, 0.5).bidirectional();
+                // link(1, 2, 3000, 0.5);
+                // link(2, 1, 3000, 0.5);
+                // link(3, 2, 3000, 0.5);
+                // link(4, 2, 3000, 0.5);
+                defaultLinks(0, 0.0);
+            }
+        };
+        Topology topology2 = new Topology() {
+            {
+                for (int i = 1; i <= NODES; i++) {
+                    node(i, "127.0.0.1", 10000 + i);
+                }
+                link(1, 2, 100, 0.5).bidirectional();
+                link(2, 3, 100, 0.5).bidirectional();
+                defaultLinks(100, 0.0);
+            }
+        };
+        Scenario scenario1 = new Scenario(Assignment3aMain.class) {
+            {
+                command(1, "D3000:R");
+                command(2, "D500:W4:D2500");
+                command(3, "D1000:R");
+            }
+        };
+        Scenario scenario2 = new Scenario(Assignment3aMain.class) {
+            {
+                command(1, "S500:BHello from 1");
+                command(2, "S10500:BHello from 2");
+                command(3, "S20500:BHello from 3");
+                command(4, "S30500:BHello from 4");
+            }
+        };
+        Scenario scenario3 = new Scenario(Assignment3aMain.class) {
+            {
+                command(1, "S500:Bdebug1");
+                command(2, "S2000:X");
+                command(3, "S2000:X");
+                command(4, "S2000:X");
+            }
+        };
+        Scenario scenario4 = new Scenario(Assignment3aMain.class) {
+            {
+                command(1, "S100:Ba1:"
+                        + "S200:Ba2:"
+                        + "S200:Ba3:"
+                        + "S200:Ba4:"
+                        + "S200:Ba5:"
+                        + "S200:Ba6:"
+                        + "S200:Ba7:"
+                        + "S200:Ba8:"
+                        + "S200:Ba9:"
+                        + "S200:Ba10");
+                command(2, "S100");
+                command(3, "S150:Bb1:"
+                        + "S250:Bb2:"
+                        + "S250:Bb3:"
+                        + "S250:Bb4:"
+                        + "S250:Bb5:"
+                        + "S250:Bb6:"
+                        + "S250:Bb7:"
+                        + "S250:Bb8:"
+                        + "S250:Bb9:"
+                        + "S250:Bb10");
+                for (int i = 4; i <= NODES; i++) {
+                    command(i, "S100");
+                }
+            }
+        };
+
+        scenario1.executeOn(topology1);
+
+        System.exit(0);
+    }
+}
---
+++ trunk/ID2203/HW1/src/se/kth/ict/id2203/Assignment3aMain.java
@@ -0,0 +1,115 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package se.kth.ict.id2203;
+
+import java.util.Set;
+import org.apache.log4j.PropertyConfigurator;
+import se.kth.ict.id2203.application.Application3a;
+import se.kth.ict.id2203.application.ApplicationInit;
+import se.kth.ict.id2203.broadcast.best.BestEffortBroadcast;
+import se.kth.ict.id2203.broadcast.best.basic.BasicBroadcast;
+import se.kth.ict.id2203.broadcast.best.basic.BasicBroadcastInit;
+import se.kth.ict.id2203.fd.pfd.PerfectFailureDetector;
+import se.kth.ict.id2203.fd.pfd.my.MyPerfectFailureDetector;
+import se.kth.ict.id2203.fd.pfd.my.MyPerfectFailureDetectorInit;
+import se.kth.ict.id2203.pp2p.PerfectPointToPointLink;
+import se.kth.ict.id2203.pp2p.delay.DelayLink;
+import se.kth.ict.id2203.pp2p.delay.DelayLinkInit;
+import se.kth.ict.id2203.registers.atomic.AtomicRegister;
+import se.kth.ict.id2203.registers.atomic.riwc.ReadImposeWriteConsult;
+import se.kth.ict.id2203.registers.atomic.riwc.RiwcInit;
+import se.sics.kompics.Component;
+import se.sics.kompics.ComponentDefinition;
+import se.sics.kompics.Fault;
+import se.sics.kompics.Handler;
+import se.sics.kompics.Kompics;
+import se.sics.kompics.address.Address;
+import se.sics.kompics.launch.Topology;
+import se.sics.kompics.network.Network;
+import se.sics.kompics.network.mina.MinaNetwork;
+import se.sics.kompics.network.mina.MinaNetworkInit;
+import se.sics.kompics.timer.Timer;
+import se.sics.kompics.timer.java.JavaTimer;
+
+/**
+ *
+ * @author julio
+ */
+public class Assignment3aMain extends ComponentDefinition {
+    static {
+        PropertyConfigurator.configureAndWatch("log4j.properties");
+    }
+    //private final static int FANOUT = 3;
+    //private final static float ALPHA = 0.5f;
+    //private final static int DELTA = 5000;
+    //private final static int MAX_ROUNDS = 2;
+    private static int selfId;
+    private static String commandScript;
+    Topology topology = Topology.load(System.getProperty("topology"), selfId);
+
+    public static void main(String[] args) {
+        selfId = Integer.parseInt(args[0]);
+        commandScript = args[1];
+
+        Kompics.createAndStart(Assignment3aMain.class);
+    }
+
+    public Assignment3aMain() {
+        // create components
+        Component time = create(JavaTimer.class);
+        Component network = create(MinaNetwork.class);
+        Component pp2p = create(DelayLink.class);
+        Component pfd = create(MyPerfectFailureDetector.class);
+        Component beb = create(BasicBroadcast.class);
+        Component riwc = create(ReadImposeWriteConsult.class);
+        Component app = create(Application3a.class);
+
+        // handle possible faults in the components
+        subscribe(faultHandler, time.control());
+        subscribe(faultHandler, network.control());
+        subscribe(faultHandler, pp2p.control());
+        subscribe(faultHandler, pfd.control());
+        subscribe(faultHandler, beb.control());
+        subscribe(faultHandler, riwc.control());
+        subscribe(faultHandler, app.control());
+
+        // initialize the components
+        Address self = topology.getSelfAddress();
+        Set<Address> neighborSet = topology.getNeighbors(self);
+        int heartbeatInterval = 1000;
+        int checkInterval = 4000;
+        int numRegisters = 1;
+
+        trigger(new MinaNetworkInit(self, 5), network.control());
+        trigger(new DelayLinkInit(topology), pp2p.control());
+        trigger(new BasicBroadcastInit(neighborSet, self), beb.control());
+        trigger(new MyPerfectFailureDetectorInit(neighborSet, self, heartbeatInterval, checkInterval), pfd.control());
+        trigger(new RiwcInit(neighborSet, self, numRegisters), riwc.control());
+        trigger(new ApplicationInit(commandScript), app.control());
+
+        // connect the components
+        connect(app.required(AtomicRegister.class), riwc.provided(AtomicRegister.class));
+        connect(app.required(Timer.class), time.provided(Timer.class));
+        
+        connect(riwc.required(BestEffortBroadcast.class), beb.provided(BestEffortBroadcast.class));
+        connect(riwc.required(PerfectPointToPointLink.class), pp2p.provided(PerfectPointToPointLink.class));
+        connect(riwc.required(PerfectFailureDetector.class), pfd.provided(PerfectFailureDetector.class));
+        
+        connect(beb.required(PerfectPointToPointLink.class), pp2p.provided(PerfectPointToPointLink.class));
+        
+        connect(pfd.required(PerfectPointToPointLink.class), pp2p.provided(PerfectPointToPointLink.class));
+        connect(pfd.required(Timer.class), time.provided(Timer.class));
+        
+        connect(pp2p.required(Network.class), network.provided(Network.class));
+        connect(pp2p.required(Timer.class), time.provided(Timer.class));
+    }
+    //handlers
+    Handler<Fault> faultHandler = new Handler<Fault>() {
+        @Override
+        public void handle(Fault fault) {
+            fault.getFault().printStackTrace(System.err);
+        }
+    };
+}
