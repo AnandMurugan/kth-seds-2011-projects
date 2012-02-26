79eaca82bce7c668a901cad0e9406cca7ab55224
igor.kostyakov@gmail.com <igor.kostyakov@gmail.com@4297f225-f6bb-08c7-5789-f87774b56954>
2012-02-26 12:50:24 +0000

    paxos consensus

--- ID2203/HW1/src/se/kth/ict/id2203/consensus/uniform/paxos/PaxosUniformConsensus.java
+++ ID2203/HW1/src/se/kth/ict/id2203/consensus/uniform/paxos/PaxosUniformConsensus.java
@@ -4,7 +4,9 @@
  */
 package se.kth.ict.id2203.consensus.uniform.paxos;
 
-import java.util.*;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
 import se.kth.ict.id2203.broadcast.beb.BestEffortBroadcast;
 import se.kth.ict.id2203.consensus.abortable.AbortableConsensus;
 import se.kth.ict.id2203.consensus.uniform.UniformConsensus;
@@ -28,6 +30,10 @@ public class PaxosUniformConsensus extends ComponentDefinition {
     //local variables
     private Address self;
     private Set<Integer> seenIds;
+    private boolean leader;
+    private Map<Integer, Object> proposal;
+    private Map<Integer, Boolean> proposed;
+    private Map<Integer, Boolean> decided;
 
     public PaxosUniformConsensus() {
         subscribe(initHandler, control);
@@ -37,10 +43,19 @@ public class PaxosUniformConsensus extends ComponentDefinition {
         @Override
         public void handle(PaxosUniformConsensusInit event) {
             self = event.getSelf();
+
+            seenIds = new TreeSet<Integer>();
+            leader = false;
         }
     };
 
     //procedures and functions
     private void initInstance(int id) {
+        if (!seenIds.contains(id)) {
+            proposal.put(id, null);
+            proposed.put(id, false);
+            decided.put(id, false);
+            seenIds.add(id);
+        }
     }
 }
