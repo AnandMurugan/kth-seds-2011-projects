c5cc52c3df7c8db55cb55658af4f37ff789f8e55
igor.kostyakov@gmail.com <igor.kostyakov@gmail.com@4297f225-f6bb-08c7-5789-f87774b56954>
2012-02-26 19:37:24 +0000

    app

---
+++ ID2203/HW1/src/se/kth/ict/id2203/Assignment4Executor.java
@@ -0,0 +1,88 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package se.kth.ict.id2203;
+
+import se.sics.kompics.launch.Scenario;
+import se.sics.kompics.launch.Topology;
+
+/**
+ *
+ * @author Igor
+ */
+public class Assignment4Executor {
+    private static final int NODES = 6;
+
+    public static void main(String[] args) {
+        Topology topology0 = new Topology() {
+            {
+                node(1, "127.0.0.1", 10001);
+                node(2, "127.0.0.1", 10002);
+                node(3, "127.0.0.1", 10003);
+                node(4, "127.0.0.1", 10004);
+
+                defaultLinks(100, 0.0);
+            }
+        };
+        Topology topologyEx1And2 = new Topology() {
+            {
+                node(1, "127.0.0.1", 10001);
+                node(2, "127.0.0.1", 10002);
+                node(3, "127.0.0.1", 10003);
+
+                defaultLinks(1000, 0.0);
+            }
+        };
+        Topology topologyEx3 = new Topology() {
+            {
+                node(1, "127.0.0.1", 10001);
+                node(2, "127.0.0.1", 10002);
+                node(3, "127.0.0.1", 10003);
+
+//                link(1, 2, 1000, 0).bidirectional();
+//                link(1, 3, 2000, 0).bidirectional();
+//                link(2, 3, 1750, 0).bidirectional();
+                //defaultLinks(1000, 0.0);
+                link(1, 2, 1000, 0).bidirectional();
+                link(1, 3, 2000, 0).bidirectional();
+                link(2, 3, 0, 0).bidirectional();
+            }
+        };
+
+
+        Scenario scenario0 = new Scenario(Assignment4Main.class) {
+            {
+                command(1, "S500");
+                command(2, "S500");
+                command(3, "S500");
+                command(4, "S500");
+            }
+        };
+        Scenario scenarioEx1 = new Scenario(Assignment4Main.class) {
+            {
+                command(1, "S30000");
+                command(2, "S500:W4:S25000");
+                command(3, "S10000:R");
+            }
+        };
+        Scenario scenarioEx2 = new Scenario(Assignment4Main.class) {
+            {
+                command(1, "S500:W5:R:S5000:R:S30000");
+                command(2, "S500:W6:R:S5000:R:S30000");
+                command(3, "S500:R:S500:R:S10000", 20000);
+            }
+        };
+        Scenario scenarioEx3 = new Scenario(Assignment4Main.class) {
+            {
+                command(1, "S500:W1:R:S500:R:S8000",000);
+                command(2, "S500:W2:R:S500:R:S8000",100);
+                command(3, "S500:W3:R:S500:R:S8000",200);
+            }
+        };
+
+        scenarioEx2.executeOn(topologyEx1And2);
+
+        System.exit(0);
+    }
+}
---
+++ ID2203/HW1/src/se/kth/ict/id2203/Assignment4Main.java
@@ -0,0 +1,118 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package se.kth.ict.id2203;
+
+import java.util.Set;
+import org.apache.log4j.PropertyConfigurator;
+import se.kth.ict.id2203.application.assignment3.Application3;
+import se.kth.ict.id2203.application.ApplicationInit;
+import se.kth.ict.id2203.broadcast.beb.BestEffortBroadcast;
+import se.kth.ict.id2203.broadcast.beb.basic.BasicBroadcast;
+import se.kth.ict.id2203.broadcast.beb.basic.BasicBroadcastInit;
+import se.kth.ict.id2203.consensus.abortable.AbortableConsensus;
+import se.kth.ict.id2203.consensus.abortable.rw.ReadWriteAbortableConsensus;
+import se.kth.ict.id2203.consensus.abortable.rw.ReadWriteAbortableConsensusInit;
+import se.kth.ict.id2203.consensus.uniform.UniformConsensus;
+import se.kth.ict.id2203.consensus.uniform.paxos.PaxosUniformConsensus;
+import se.kth.ict.id2203.consensus.uniform.paxos.PaxosUniformConsensusInit;
+import se.kth.ict.id2203.detectors.leader.eld.EventualLeaderDetector;
+import se.kth.ict.id2203.detectors.leader.eld.failnoisy.FailNoisyEventualLeaderDetector;
+import se.kth.ict.id2203.detectors.leader.eld.failnoisy.FailNoisyEventualLeaderDetectorInit;
+import se.kth.ict.id2203.link.pp2p.PerfectPointToPointLink;
+import se.kth.ict.id2203.link.pp2p.delay.DelayLink;
+import se.kth.ict.id2203.link.pp2p.delay.DelayLinkInit;
+import se.kth.ict.id2203.registers.atomic.AtomicRegister;
+import se.kth.ict.id2203.registers.atomic.riwcm.ReadImposeWriteConsultMajorityAtomicRegisterInit;
+import se.sics.kompics.*;
+import se.sics.kompics.address.Address;
+import se.sics.kompics.launch.Topology;
+import se.sics.kompics.network.Network;
+import se.sics.kompics.network.mina.MinaNetwork;
+import se.sics.kompics.network.mina.MinaNetworkInit;
+import se.sics.kompics.timer.Timer;
+import se.sics.kompics.timer.java.JavaTimer;
+
+/**
+ *
+ * @author Igor
+ */
+public class Assignment4Main extends ComponentDefinition {
+    static {
+        PropertyConfigurator.configureAndWatch("log4j.properties");
+    }
+    private static final long TIME_DELAY = 2000;
+    private static final long DELTA = 500;
+    private static int selfId;
+    private static String commandScript;
+    Topology topology = Topology.load(System.getProperty("topology"), selfId);
+
+    public static void main(String[] args) {
+        selfId = Integer.parseInt(args[0]);
+        commandScript = args[1];
+
+        Kompics.createAndStart(Assignment4Main.class);
+    }
+
+    public Assignment4Main() {
+        // create components
+        Component time = create(JavaTimer.class);
+        Component network = create(MinaNetwork.class);
+        Component pp2p = create(DelayLink.class);
+        Component beb = create(BasicBroadcast.class);
+        Component eld = create(FailNoisyEventualLeaderDetector.class);
+        Component ac = create(ReadWriteAbortableConsensus.class);
+        Component uc = create(PaxosUniformConsensus.class);
+        Component app = create(Application3.class);
+
+        // handle possible faults in the components
+        subscribe(faultHandler, time.control());
+        subscribe(faultHandler, network.control());
+        subscribe(faultHandler, pp2p.control());
+        subscribe(faultHandler, beb.control());
+        subscribe(faultHandler, eld.control());
+        subscribe(faultHandler, ac.control());
+        subscribe(faultHandler, uc.control());
+        subscribe(faultHandler, app.control());
+
+        // initialize the components
+        Address self = topology.getSelfAddress();
+        Set<Address> neighborSet = topology.getNeighbors(self);
+        Set<Address> all = topology.getAllAddresses();
+
+        trigger(new MinaNetworkInit(self, 5), network.control());
+        trigger(new DelayLinkInit(topology), pp2p.control());
+        trigger(new BasicBroadcastInit(neighborSet, self), beb.control());
+        trigger(new FailNoisyEventualLeaderDetectorInit(all, self, TIME_DELAY, DELTA), eld.control());
+        trigger(new ReadWriteAbortableConsensusInit(self, all.size()), ac.control());
+        trigger(new PaxosUniformConsensusInit(self), uc.control());
+        trigger(new ApplicationInit(commandScript), app.control());
+
+        // connect the components
+        connect(app.required(UniformConsensus.class), uc.provided(UniformConsensus.class));
+        connect(app.required(Timer.class), time.provided(Timer.class));
+
+        connect(uc.required(BestEffortBroadcast.class), beb.provided(BestEffortBroadcast.class));
+        connect(uc.required(AbortableConsensus.class), ac.provided(AbortableConsensus.class));
+        connect(uc.required(EventualLeaderDetector.class), eld.provided(EventualLeaderDetector.class));
+
+        connect(ac.required(BestEffortBroadcast.class), beb.provided(BestEffortBroadcast.class));
+        connect(ac.required(PerfectPointToPointLink.class), pp2p.provided(PerfectPointToPointLink.class));
+
+        connect(beb.required(PerfectPointToPointLink.class), pp2p.provided(PerfectPointToPointLink.class));
+
+        connect(eld.required(PerfectPointToPointLink.class), pp2p.provided(PerfectPointToPointLink.class));
+        connect(eld.required(Timer.class), time.provided(Timer.class));
+
+        connect(pp2p.required(Network.class), network.provided(Network.class));
+        connect(pp2p.required(Timer.class), time.provided(Timer.class));
+    }
+    //handlers
+    Handler<Fault> faultHandler = new Handler<Fault>() {
+        @Override
+        public void handle(Fault fault) {
+            fault.getFault().printStackTrace(System.err);
+        }
+    };
+}
