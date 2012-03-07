96faf7789284f01715da2627bb49d45f821317f8
m.anandsp@gmail.com <m.anandsp@gmail.com@4297f225-f6bb-08c7-5789-f87774b56954>
2012-03-07 21:46:20 +0000

    adding view claims

---
+++ ID2208/PROJECT/src/java/claims/view/ClaimManager.java
@@ -0,0 +1,78 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package claims.view;
+
+import claims.controller.ClaimsFacade;
+import claims.model.Claim;
+import javax.inject.Named;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import javax.ejb.EJB;
+import javax.enterprise.context.RequestScoped;
+
+/**
+ *
+ * @author Anand
+ */
+@Named(value = "claimManager")
+@RequestScoped
+public class ClaimManager implements Serializable {
+
+    @EJB
+    private ClaimsFacade claimsFacade;
+    
+    private String id;
+    private String carRegNo;
+    private String owner;
+    private String carValue;
+    private String status_desc;
+    
+    private List<Claim> claimsList = new ArrayList<Claim>();
+
+    public List<Claim> getClaimsList() {
+        return claimsList;
+    }
+
+    public void setClaimsList(List<Claim> claimsList) {
+        this.claimsList = claimsList;
+    }
+
+    public void setCarRegNo(String carRegNo) {
+        this.carRegNo = carRegNo;
+    }
+
+    public void setCarValue(String carValue) {
+        this.carValue = carValue;
+    }
+
+    public void setClaimId(String claimId) {
+        this.id = claimId;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public void setStatus(String status) {
+        this.status_desc = status;
+    }
+    
+    public boolean isListNotEmpty() {
+        refreshPage();
+        if (claimsList.size() > 0) {
+            return true;
+        }
+        return false;
+    }
+    
+    private void refreshPage() {
+        setClaimsList(claimsFacade.getClaimsList());
+    }
+    
+    /** Creates a new instance of ClaimManager */
+    public ClaimManager() {
+    }
+}
