4c655c6c2b936e94669bc65477118d8efdc5d3b5
m.anandsp@gmail.com <m.anandsp@gmail.com@4297f225-f6bb-08c7-5789-f87774b56954>
2012-03-08 13:04:51 +0000

    adding find all

--- trunk/ID2208/PROJECT/src/java/claims/controller/ClaimsFacade.java
+++ trunk/ID2208/PROJECT/src/java/claims/controller/ClaimsFacade.java
@@ -6,6 +6,7 @@ package claims.controller;
 
 import claims.model.Claim;
 import client.ClaimsClient;
+import java.util.ArrayList;
 import java.util.List;
 import javax.ejb.Stateless;
 import javax.ejb.LocalBean;
@@ -19,17 +20,22 @@ import javax.ejb.LocalBean;
 public class ClaimsFacade {
 
     private ClaimsClient client;
-    
+    private final Double factor = 100000.00;
+    private List<Claim> claimLst;
+
     public ClaimsFacade() {
         client = new ClaimsClient();
     }
 
-    public Claim getClaim(String claimId){
+    public Claim getClaim(String claimId) {
         return client.find_XML(Claim.class, claimId);
     }
-    
+
     public void submitClaim(String carRegNo) {
         Claim claimObj = new Claim(carRegNo);
+        claimObj.setType("simple");
+        claimObj.setStatus_code(0);
+        claimObj.setStatus_desc("initiate claim");
         client.create_XML(claimObj);
     }
 
@@ -38,10 +44,36 @@ public class ClaimsFacade {
         claimObj.setCar_model(carModel);
         claimObj.setOwner(owner);
         claimObj.setCar_year(carYear);
+        if (carValue != null) {
+            if (carValue < factor) {
+                claimObj.setType("simple");
+            } else {
+                claimObj.setType("complex");
+            }
+        } else {
+            claimObj.setType("simple");
+        }
+        claimObj.setStatus_code(0);
+        claimObj.setStatus_desc("initiate claim");
         client.create_XML(claimObj);
     }
 
     public List<Claim> getClaimsList() {
         return client.findAll_XML(Claim.class);
+    }
+
+    public List<String> getClaimIdList() {
+        List<String> claimIdLst = new ArrayList<String>();
+        claimLst = client.findAll_XML(Claim.class);
+        if(claimLst!=null && claimLst.size()>0){
+            for(Claim claim:claimLst){
+                claimIdLst.add(claim.getId().toString());
+            }
+        }
+        return claimIdLst;
+    }
+
+    public void approveClaim(Claim claim) {
+        claim.setStatus_code(Integer.MIN_VALUE);
     }
 }
