dc6b68992324d5ca32490b0024ace44367e1f6af
m.anandsp@gmail.com <m.anandsp@gmail.com@4297f225-f6bb-08c7-5789-f87774b56954>
2012-03-08 13:21:02 +0000

    PUT resource

--- trunk/ID2208/PROJECT/src/java/claims/view/EvaluationClaim.java
+++ trunk/ID2208/PROJECT/src/java/claims/view/EvaluationClaim.java
@@ -20,7 +20,7 @@ import javax.faces.event.AjaxBehaviorEvent;
 @Named(value = "evaluationClaim")
 @RequestScoped
 public class EvaluationClaim {
-
+    
     @EJB
     ClaimsFacade claimFacade;
     private String id;
@@ -32,7 +32,7 @@ public class EvaluationClaim {
     private String owner;
     private String type;
     List<String> claimIdLst = new ArrayList<String>();
-
+    
     public List<String> getClaimIdLst() {
         claimIdLst = claimFacade.getClaimIdList();
         if (claimIdLst.size() > 0) {
@@ -40,21 +40,30 @@ public class EvaluationClaim {
         }
         return claimIdLst;
     }
-
+    
     private void refreshPage() {
         if (claimIdLst.size() > 0) {
             id = claimIdLst.get(0).toString();
             retrieveClaimDetails();
         }
     }
-
-    public void approve(){
-       claimFacade.approveClaim(claim); 
+    
+    public void approve() {
+        claimFacade.approveClaim(claim);        
     }
+
+    public void reject() {
+        claimFacade.rejectClaim(claim);        
+    }
+
+    public void notifyClaimant() {
+        claimFacade.notifyClaimant(claim);        
+    }
+
     public void refreshClaimDetails(AjaxBehaviorEvent e) {
         retrieveClaimDetails();
     }
-
+    
     private void retrieveClaimDetails() {
         try {
             claim = claimFacade.getClaim(id);
@@ -68,71 +77,71 @@ public class EvaluationClaim {
             handleException(e1);
         }
     }
-
+    
     private void handleException(Exception e) {
         e.printStackTrace(System.err);
     }
-
+    
     public String getId() {
         return id;
     }
-
+    
     public void setId(String id) {
         this.id = id;
     }
-
+    
     public String getCarModel() {
         return carModel;
     }
-
+    
     public void setCarModel(String carModel) {
         this.carModel = carModel;
     }
-
+    
     public String getCarRegNo() {
         return carRegNo;
     }
-
+    
     public void setCarRegNo(String carRegNo) {
         this.carRegNo = carRegNo;
     }
-
+    
     public Double getCarValue() {
         return carValue;
     }
-
+    
     public void setCarValue(Double carValue) {
         this.carValue = carValue;
     }
-
+    
     public Claim getClaim() {
         return claim;
     }
-
+    
     public void setClaim(Claim claim) {
         this.claim = claim;
     }
-
+    
     public String getOwner() {
         return owner;
     }
-
+    
     public void setOwner(String owner) {
         this.owner = owner;
     }
-
+    
     public String getStatus() {
         return status;
     }
-
+    
     public void setStatus(String status) {
         this.status = status;
     }
-
+    
     public String getType() {
         return type;
     }
-
+    
     public void setType(String type) {
         this.type = type;
     }
